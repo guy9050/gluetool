4e7525c9b79f58dd024f74706a5755c4617251db
Miroslav Vadkerti <mvadkert@redhat.com>
2017-01-04 13:42:19 +0100

    rpmdiff-job: merge comparison and analysis module in one

---
+++ libci/modules/static-analysis/rpmdiff/rpmdiff-job.py
@@ -0,0 +1,84 @@
+import os
+import subprocess
+from libci import Module
+from libci import libciError
+from libci import utils
+
+# Jenkins Job Builder YAML
+CMP_JOB_NAME = 'ci-rpmdiff-comparison'
+CMP_JJB_YAML = 'ci-rpmdiff-comparison.yaml'
+ANL_JOB_NAME = 'ci-rpmdiff-analysis'
+ANL_JJB_YAML = 'ci-rpmdiff-analysis.yaml'
+
+# required cmdline tools
+REQUIRED_CMDS = ['jenkins-jobs']
+
+
+class CIRpmdiffJob(Module):
+    """
+CI RPMdiff job module
+
+This module provides a Jenkins job for RPMdiff analysis and comparison testing.
+It is able to create these jobs and also execute it. By default it reads the
+Brew task id from the environment. It is expected to call this module from a
+job, where id is exported by the redhat-ci-plugin. The id can be
+also passed as an argument.
+
+The jenkins jobs are defined via a Jenkins Job Builder yaml files:
+    ci-rpmdiff-analysis.yaml
+    ci-rpmdiff-comparison.yaml
+
+Important note:
+This module requires an available Jenkins connection - via the jenkins module.
+"""
+
+    name = 'rpmdiff-job'
+    description = 'Create and run RPMdiff analysis or comparison job'
+
+    options = {
+        'id': {
+            'help': 'Brew task id',
+        },
+        'type': {
+            'help': 'Test type: analysis or comparison',
+            'choices': ['analysis', 'comparison'],
+        }
+    }
+    required_options = ['type']
+
+    def sanity(self):
+        # check for jjb
+        utils.check_for_commands(REQUIRED_CMDS)
+
+        # check if jjb yaml exists
+        if self.option('type') == 'analysis':
+            self.job_name = CMP_JOB_NAME
+            self.yaml = os.path.join(self.data_path, CMP_JJB_YAML)
+        else:
+            self.job_name = ANL_JOB_NAME
+            self.yaml = os.path.join(self.data_path, ANL_JJB_YAML)
+        if not os.path.exists(self.yaml):
+            raise libciError('job yaml not found in \'{}\''.format(self.yaml))
+
+        # check for available id
+        self.id = self.option('id') or os.environ['id']
+        if not self.id:
+            raise libciError('id not found in environment')
+
+    def execute(self):
+        jenkins = self.shared('jenkins')
+        if not jenkins:
+            raise libciError('no jenkins connection found')
+
+        out = subprocess.check_output(['jenkins-jobs', 'update',
+                                       self.yaml],
+                                      stderr=subprocess.STDOUT)
+        # TODO parse JJB output and inform about the update
+        self.debug(out)
+
+        jenkins[self.job_name].invoke(self.id, build_params={
+                                        'id': self.id
+                                     })
+        msg = 'invoked job \'{}\' with build params '.format(self.job_name)
+        msg += '\'id={}\''.format(self.id)
+        self.info(msg)
