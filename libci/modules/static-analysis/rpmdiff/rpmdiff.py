d46e08b4154dea66eed04ee2e4d6cccde7fe23d8
Miroslav Vadkerti <mvadkert@redhat.com>
2017-01-19 16:00:03 +0100

    rpmdiff: fix #11 - refactoring
    Remove unneded stuff, we can now use rpmdiff-remote
    to query runinfo.
    
    Signed-off-by: Miroslav Vadkerti <mvadkert@redhat.com>

--- libci/modules/static-analysis/rpmdiff/rpmdiff.py
+++ libci/modules/static-analysis/rpmdiff/rpmdiff.py
@@ -1,93 +1,23 @@
+import json
 import re
 import subprocess
 import time
-import kerberos
-import requests
-from requests.packages.urllib3.exceptions import InsecureRequestWarning
 from libci import Module
 from libci import CiError
 from libci import utils
 
-RPMDIFF_URL = "https://rpmdiff.engineering.redhat.com"
-RPMDIFF_BASELINE_URL = "http://rpmdiff-baseline.usersys.redhat.com"
 RPMDIFF_PASS_STATES = ["Passed", "Info", "Waived", "Needs inspection"]
-RPMDIFF_KERBEROS_PRINCIPAL = "HTTP@rpmdiff-web-01.host.prod.eng.bos.redhat.com"
-
 REQUIRED_CMDS = ['rpmdiff-remote']
-
-
-class RPMDiffTask(object):
-    def __init__(self, rpmdiff_task_id):
-        self.rpmdiff_url = RPMDIFF_URL
-        self.api_url = self.rpmdiff_url + "/json/"
-        self.login_url = self.rpmdiff_url + "/auth/login/?next=/"
-        self.task_id = int(rpmdiff_task_id)
-        self._json_data = None
-        self.url = self.rpmdiff_url + "/run/" + str(self.task_id)
-
-    @staticmethod
-    def get_kerberos_ticket():
-        _, krb_context = kerberos.authGSSClientInit(RPMDIFF_KERBEROS_PRINCIPAL)
-        kerberos.authGSSClientStep(krb_context, "")
-        return kerberos.authGSSClientResponse(krb_context)
-
-    def _fetch_json(self):
-        payload = {
-            "id": "jsonrpc",
-            "jsonrpc": "1.0",
-            "method": "getResult",
-            "params": [self.task_id]
-        }
-        headers = {
-            "Authorization": "Negotiate " + self.get_kerberos_ticket()
-        }
-        requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
-        request = requests.post(self.api_url, json=payload, headers=headers,
-                                verify=False)
-        if request.status_code != 200:
-            msg = "Http status code {} ".format(request.status_code)
-            msg += "in 'rpmdiff' execution"
-            raise CiError("Http status code %s in 'rpmdiff' execution" % request.status_code)
-        self._json_data = request.json()
-
-    def _json(self):
-        if self._json_data is None:
-            self._fetch_json()
-        return self._json_data
-
-    @property
-    def state(self):
-        return self._json()['result']['state']
-
-    @property
-    def result(self):
-        return self._json()['result']['result']
-
-    @property
-    def data(self):
-        return self._json()['result']['data']
-
-    @property
-    def error(self):
-        if "error" not in self._json()['result']:
-            return ""
-        return self._json()['result']['error']
-
-    @property
-    def finished(self):
-        return self.state == "CLOSED"
-
-    @property
-    def passed(self):
-        return self.finished and self.result in RPMDIFF_PASS_STATES
 
 
 class CIRpmdiff(Module):
     """
-CI RPMdiff module
+    CI RPMdiff module
 
-This module schedules an RPMdiff comparison waits until it is finished.
-"""
+    This module schedules an RPMdiff run, waits until it is finished and reports
+    results. The run is considered as passed if its overal score is one of:
+    {}
+    """.format(', '.join(RPMDIFF_PASS_STATES))
 
     name = 'rpmdiff'
     description = 'Run RPMdiff analysis or comparison'
@@ -96,26 +26,26 @@ This module schedules an RPMdiff comparison waits until it is finished.
         'blacklist': {
             'help': 'A comma seaparted list of blacklisted package names',
         },
-        'id': {
-            'help': 'Brew task id',
-            'type': int,
-        },
         'type': {
             'help': 'Test type: analysis or comparison',
             'choices': ['analysis', 'comparison'],
+        },
+        'url': {
+            'help': 'RPMdiff Hub URL',
         }
     }
-    required_options = ['id', 'type']
+    required_options = ['type']
 
     brew_task = None
     check_interval = 60
     max_timeout = 3600 * 24
+    rpmdiff_cmd = None
 
     def sanity(self):
         utils.check_for_commands(REQUIRED_CMDS)
 
     @staticmethod
-    def _parse_task_id(string):
+    def _parse_run_id(string):
         # parse JSON once BZ#1405962 is fixed
         match = re.search(r"u'run_id': (\d+)", string)
         if not match:
@@ -132,32 +62,23 @@ This module schedules an RPMdiff comparison waits until it is finished.
             raise CiError(msg)
         return match.group(1)
 
+    def _get_runinfo(self, task_id):
+        command = self.rpmdiff_cmd + ["runinfo", task_id]
+        return json.loads(self._run_command(command))
+
     def _wait_until_finished(self, task_id):
         start_time = time.time()
-        task = RPMDiffTask(task_id)
-        self.verbose("RPMdiff task [{}] state: {}".format(task_id, task.state))
-        while not task.finished:
-            task = RPMDiffTask(task_id)
+        runinfo = self._get_runinfo(task_id)
+        self.verbose("RPMdiff task [{}] state: {}".format(task_id, runinfo['overall_score']['description']))
+        while runinfo['overall_score']['description'] in ['Running', 'Queued for test']:
             if (time.time() - start_time) > self.max_timeout:
-                raise CiError("Timeout of 'rpmdiff' execution")
+                raise CiError("Waiting for RPMdiff results timed out ")
             time.sleep(self.check_interval)
+            runinfo = self._get_runinfo(task_id)
+        return runinfo
 
-    def _comparison_command(self, nvr_baseline):
-        if self.brew_task.scratch:
-            return ["rpmdiff-remote", "schedule", str(self.brew_task.task_id),
-                    "--baseline", nvr_baseline]
-        else:
-            return ["rpmdiff-remote", "schedule", self.brew_task.nvr,
-                    "--baseline", nvr_baseline]
-
-    def _analysis_command(self):
-        if self.brew_task.scratch:
-            return ["rpmdiff-remote", "schedule", str(self.brew_task.task_id)]
-        else:
-            return ["rpmdiff-remote", "schedule", self.brew_task.nvr]
-
-    def _run_command(self, command, wait_until_finished=True):
-        msg = "schedule command: {}".format(subprocess.list2cmdline(command))
+    def _run_command(self, command):
+        msg = "command: {}".format(subprocess.list2cmdline(command))
         self.verbose(msg)
         process = subprocess.Popen(command, stdout=subprocess.PIPE,
                                    stderr=subprocess.PIPE)
@@ -166,33 +87,40 @@ This module schedules an RPMdiff comparison waits until it is finished.
         self.debug("stdout: {}".format(p_out or "no output"))
         self.debug("stderr: {}".format(p_err or "no output"))
         if p_status > 0:
-            msg = "Failure during 'rpmdiff' command execution: {}".format(p_err)
+            msg = "Failure during command execution: {}".format(p_err)
             raise CiError(msg)
-        task_id = self._parse_task_id(p_out)
-        self.info("web url: {}".format(self._parse_web_url(p_out)))
-        if wait_until_finished:
-            self._wait_until_finished(task_id)
-        return task_id
+        return p_out
 
-    def _run_comparison(self, nvr_baseline, wait_until_finished=True):
-        command = self._comparison_command(nvr_baseline)
-        task_id = self._run_command(command, wait_until_finished)
-        return RPMDiffTask(task_id)
+    def _run_rpmdiff(self, test_type, nvr_baseline=None):
+        if self.brew_task.scratch:
+            command = self.rpmdiff_cmd + ["schedule", str(self.brew_task.task_id)]
+        else:
+            command = self.rpmdiff_cmd + ["schedule", self.brew_task.nvr]
 
-    def _run_analysis(self, wait_until_finished=True):
-        command = self._analysis_command()
-        task_id = self._run_command(command, wait_until_finished)
-        return RPMDiffTask(task_id)
+        if test_type == 'comparison':
+            command += ["--baseline", nvr_baseline]
+
+        out = self._run_command(command)
+        # once we have valid JSON, we can parse taht here
+        self.info("web url: {}".format(self._parse_web_url(out)))
+        run_id = self._parse_run_id(out)
+        return self._wait_until_finished(run_id)
 
     def execute(self):
-        task_id = self.option('id')
         test_type = self.option('type')
         blacklist = self.option('blacklist')
+        url = self.option('url')
+        latest = None
+
+        # override url if requested
+        self.rpmdiff_cmd = ['rpmdiff-remote']
+        if url:
+            self.rpmdiff_cmd += ['--hub-url', url]
 
         # get a brew task instance
-        self.brew_task = self.shared('brew_task', task_id)
+        self.brew_task = self.shared('brew_task')
         if not self.brew_task:
-            raise CiError('no brew connection found')
+            raise CiError('no brew build found, did you run brew module?')
 
         # get target from the brewtask
         target = self.brew_task.target.target
@@ -206,24 +134,24 @@ This module schedules an RPMdiff comparison waits until it is finished.
                 self.info(msg)
                 return
 
-        if test_type == 'analysis':
-            msg = 'running {} for '.format(test_type)
-            if self.brew_task.scratch is True:
-                msg += 'scratch '
-            msg += 'build of \'{}\' '.format(self.brew_task.nvr)
-            msg += 'with build-target \'{}\''.format(target)
-            self.info(msg)
-            result = self._run_analysis()
-        else:
+        msg = '{} for '.format(test_type)
+        if self.brew_task.scratch is True:
+            msg += 'scratch '
+        msg += 'build of \'{}\' '.format(self.brew_task.nvr)
+
+        if test_type == 'comparison':
             latest = self.brew_task.latest
             if not latest:
                 raise CiError('could not find baseline for this build')
-            msg = 'running {} for '.format(test_type)
-            if self.brew_task.scratch is True:
-                msg += 'scratch '
             msg += 'build of \'{}\' '.format(self.brew_task.nvr)
             msg += 'compared to \'{}\' '.format(latest)
-            msg += 'with build target \'{}\''.format(target)
-            self.info(msg)
-            result = self._run_comparison(latest)
-        self.info('result: {}'.format(result.result))
+
+        msg += 'with build-target \'{}\''.format(target)
+        self.info(msg)
+
+        runinfo = self._run_rpmdiff(test_type, latest)
+        if runinfo['overall_score']['description'] in RPMDIFF_PASS_STATES:
+            result = 'Passed'
+        else:
+            result = 'Failed'
+        self.info('result: {}'.format(result))
