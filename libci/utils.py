8ec02733a034165882f0c1d96f2e15785cf09959
Miroslav Vadkerti <mvadkert@redhat.com>
2017-01-24 13:12:08 +0100

    utils.py: run command in shell
    To make sure it works also with 'command' bash built-in
    command.
    
    Signed-off-by: Miroslav Vadkerti <mvadkert@redhat.com>

--- libci/utils.py
+++ libci/utils.py
@@ -15,7 +15,7 @@ except ImportError:
 def check_for_commands(cmds):
     """ Checks if all commands in list cmds are valid """
     for cmd in cmds:
-        p = subprocess.Popen(['command', '-v', cmd], stdout=DEVNULL)
+        p = subprocess.Popen(['command', '-v', cmd], stdout=DEVNULL, shell=True)
         retcode = p.wait()
         p.communicate()
         if retcode != 0:
