e247846110b38c4d9db19f3cdcf4eaaf23ba538a
Miroslav Vadkerti <mvadkert@redhat.com>
2017-02-02 22:43:19 +0100

    guess-compose: add new module for guessing distro
    The distro is guessed from the build target of the
    brew task.
    
    Signed-off-by: Miroslav Vadkerti <mvadkert@redhat.com>

---
+++ libci/modules/helpers/guess_compose.py
@@ -0,0 +1,42 @@
+import re
+from libci import CIError, Module
+
+class CIGuessCompose(Module):
+    """
+    Guess compose from build target. Currently these translations
+    are supported (sed -r syntax):
+
+    s/(staging-)?(rhel-[0-9]+)(.[0-9]+)?-?(z)?-candidate/\2\3.\4/
+    """
+    name = 'guess-distro'
+    description = 'Guess distro from build target of a brew build'
+    options = {
+        'distro': {
+            'help': 'Force usage of given compose',
+        }
+    }
+    shared_functions = ['distro']
+    distro_value = None
+
+    def distro(self):
+        ""
+        return self.distro_value
+
+    def execute(self):
+        self.distro_value = self.option('distro')
+        if self.distro_value is not None:
+            self.info("forcing distro to '{}'".format(self.distro_value))
+            return
+
+        task = self.shared('brew_task')
+        if task is None:
+            raise CIError('no brew build found, did you run brew module')
+        m = re.match(r'(staging-)?(rhel-[0-9]+)(.[0-9]+)?-?(z)?-candidate', task.target.target)
+        if m:
+            self.distro_value = '{}{}{}'.format(m.group(2),
+                                                m.group(3) or '',
+                                                '.' + m.group(4) if m.group(4) else '')
+            self.info("guessed distro '{}' from build target '{}'".format(self.distro_value,
+                                                                          task.target.target))
+        else:
+            raise CIError("could not translate build target '{}' to compose".format(task.target.target))
