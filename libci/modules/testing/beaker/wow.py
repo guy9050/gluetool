4ebce77a7464ef1286c104e139239782399d926a
Milos Prchlik <mprchlik@redhat.com>
2017-03-01 15:38:09 +0100

    Publish rich wow summary for use by frontends
    wow module uses tcms-results to process beaker jobs, and using patched
    tcms-results internals to gather data about tests that ran. This data
    set is then stored as an artifacts, available to public world.
    
    Motivated by need to connect bblaskovic's frontend to citool powered
    pipeline.

--- libci/modules/testing/beaker/wow.py
+++ libci/modules/testing/beaker/wow.py
@@ -1,12 +1,139 @@
 import os
 import shlex
+import sys
 import urlparse
-import bs4
+import imp
+
 from libci import CIError, CICommandError, Module, utils
-from libci.utils import run_command
+from libci.utils import run_command, log_blob, format_dict
 
 
-REQUIRED_COMMANDS = ['bkr', 'beaker-jobwatch']
+REQUIRED_COMMANDS = ['bkr', 'beaker-jobwatch', '/bin/tcms-results']
+
+DEFAULT_BEAKER_RESULTS_FILE = 'beaker-results.json'
+
+# Import tcms-results as a module so we can reuse its code
+try:
+    # pylint: disable=invalid-name
+    tcms_results = imp.load_source('tcms_results', '/bin/tcms-results')
+
+except ImportError as e:
+    raise CIError('Cannot import tcms-results: {}'.format(str(e)))
+
+
+class TaskAggregator(tcms_results.TaskAggregator):
+    # pylint: disable=too-few-public-methods
+
+    """
+    This class makes use of tcms-result's unique gift of parsing and processing
+    of beaker data, and simply observes and stores processed data, to let us
+    simply store the gathered data and present them to the world.
+    """
+
+    WOW_MODULE = None
+    WOW_STORAGE = {}
+
+    # pylint: disable=invalid-name
+    def recordResult(self, task, caserun):
+        """
+        Overrides original method that does the actual processing of results.
+        Our code lets it do its job, and when its done, we just store some
+        of that data into our storage.
+        """
+
+        # call the original method to fulfill tcms-results duty
+        super(TaskAggregator, self).recordResult(task, caserun)
+
+        if task.status != 'Completed':
+            self._wow_module.warn('Task {0} not completed'.format(task.name))
+            return
+
+        DEBUG = TaskAggregator.WOW_MODULE.debug
+        storage = TaskAggregator.WOW_STORAGE
+
+        xml = task.xml
+        journal = tcms_results.JournalParser(task)
+
+        DEBUG("task: '{}'".format(task.name))
+        log_blob(DEBUG, 'task XML', xml.toprettyxml())
+        log_blob(DEBUG, 'task journal', journal.getDetails())
+
+        machine = xml.getElementsByTagName('roles')[0].getElementsByTagName('system')[0]
+
+        if task.name not in storage:
+            storage[task.name] = []
+
+        data = {
+            'name': task.name,
+            'bkr_recipe_id': int(task.recipe.id),
+            'bkr_distrovariant': str(task.recipe.environment),
+            'bkr_task_id': int(xml.attributes['id'].value),
+            'bkr_version': xml.attributes['version'].value,
+            'bkr_arch': task.recipe.arch,
+            'bkr_status': task.status,
+            'bkr_result': task.result,
+            'bkr_host': machine.attributes['value'].value,
+            'bkr_duration': 0,
+            'bkr_params': [],
+            'bkr_packages': [],
+            'bkr_phases': {},
+            'bkr_logs': []
+        }
+
+        # convert test duration in a form of %h:%m:%s to simple integer (sec)
+        # note: duration doesn't always need to be there, e.g. when "External
+        # Watchdog Expired"
+        duration = xml.attributes.get('duration')
+        if duration is not None:
+            data['bkr_duration'] = 0
+
+            days_and_hours = duration.value.split(',')  # "1 day, 23:51:43"
+            if len(days_and_hours) > 1:
+                data['bkr_duration'] += int(days_and_hours.pop(0).split(' ')[0]) * 24 * 60 * 60
+
+            _chunks = [int(_chunk) for _chunk in days_and_hours[0].split(':')]
+            data['bkr_duration'] += _chunks[0] * 60 * 60 + _chunks[1] * 60 + _chunks[2]
+
+        # store params, if any
+        params_node = xml.getElementsByTagName('params')
+        if len(params_node) > 0:
+            for param in params_node[0].getElementsByTagName('param'):
+                # this is caserun.id, safe to ignore
+                if param.attributes['name'].value == 'CASERUNS':
+                    continue
+
+                data['bkr_params'].append('%s="%s"' % (param.attributes['name'].value, param.attributes['value'].value))
+
+        # store packages, only unique names
+        packages = {}
+        for phase, phase_packages in journal.getDetails().iteritems():
+            map(packages.__setitem__, phase_packages, [])
+
+        data['bkr_packages'] = [k.strip() for k in sorted(packages.keys())]
+
+        # store phases
+        results_nodes = xml.getElementsByTagName('results')
+        if results_nodes:
+            for result_node in results_nodes[0].getElementsByTagName('result'):
+                phase = result_node.attributes['path'].value
+                result = result_node.attributes['result'].value
+
+                data['bkr_phases'][phase] = result
+
+        # store log links
+        logs_nodes = xml.getElementsByTagName('logs')
+        if logs_nodes:
+            for log_node in logs_nodes[0].getElementsByTagName('log'):
+                data['bkr_logs'].append({
+                    'href': log_node.attributes['href'].value,
+                    'name': log_node.attributes['name'].value
+                })
+
+        storage[task.name].append(data)
+
+
+# Replace the original TaskAggregator class with our custom version
+tcms_results.TaskAggregator = TaskAggregator
 
 
 class CIWow(Module):
@@ -14,14 +141,18 @@ class CIWow(Module):
     This module just wraps beaker workflow tomorrow and injects --distro
     from shared 'distro' function if available.
     """
-    name = 'wow'
 
+    name = 'wow'
     options = {
         'wow-options': {
             'help': 'Additional options for workflow-tomorrow'
         },
         'jobwatch-options': {
             'help': 'Additional options for beaker-jobwatch'
+        },
+        'beaker-results': {
+            'help': 'Name of the file with beaker results JSON',
+            'default': DEFAULT_BEAKER_RESULTS_FILE
         }
     }
 
@@ -34,6 +165,161 @@ class CIWow(Module):
 
     def sanity(self):
         utils.check_for_commands(REQUIRED_COMMANDS)
+
+    def _run_wow(self, task, distro, options):
+        """
+        Run workflow-tomorrow to create beaker jobs, using options we
+        got from the user.
+
+        :param task: brew task info, as returned by `brew_task` shared function
+        :param str distro: distribution to install.
+        :param list options: additional options, usualy coming from wow-options option.
+        :returns: libci.utils.ProcessOutput with the output of w-t.
+        """
+
+        distro_option = ['--distro={}'.format(distro)] if distro else []
+        brew_option = ['--brew-task={}'.format(task.task_id)] if task is not None else []
+
+        whiteboard = 'CI run {} brew task id {} build target {}'.format(task.nvr, task.task_id, task.target.target)
+
+        # wow
+        command = [
+            'bkr', 'workflow-tomorrow',
+            '--id',
+            '--whiteboard', whiteboard,
+            '--no-reserve'
+        ] + distro_option + brew_option + options
+
+        self.info("running 'workflow-tomorrow':\n{}".format(utils.format_command_line([command])))
+
+        try:
+            return run_command(command)
+
+        except CICommandError as exc:
+            # Check for most common causes, and raise soft error where necessary
+            soft = False
+
+            if 'No relevant tasks found in test plan' in exc.output.stderr:
+                soft = True
+
+            raise CIError("Failure during 'wow' execution: {}".format(exc.output.stderr), soft=soft)
+
+    def _run_jobwatch(self, jobs, options):
+        """
+        Start beaker-jobwatch, to baby-sit our jobs, and wait for its completion.
+
+        :param list job: list of job IDs.
+        :param list options: additional options, usualy coming from jobwatch-options option.
+        :returns: libci.utils.ProcessOutput with the output of beaker-jobwatch.
+        """
+
+        command = [
+            'beaker-jobwatch',
+            '--skip-broken-machines'
+        ] + options + ['--job={}'.format(job) for job in jobs]
+
+        self.info("running 'beaker-jobwatch':\n{}".format(utils.format_command_line([command])))
+
+        command = ['bash', '-c', '{} | tee beaker-jobwatch.log'.format(' '.join(command))]
+
+        try:
+            output = run_command(command, stdout=utils.PARENT, stderr=utils.PARENT)
+
+        except CICommandError as exc:
+            raise CIError("Failure during 'jobwatch' execution: {}".format(exc.output.stderr))
+
+        self.debug('output of beaker-jobwatch is stored in beaker-jobwatch.log')
+
+        with open('beaker-jobwatch.log', 'r') as f:
+            output.stdout = f.read().strip()
+
+        return output
+
+    def _process_jobs(self, jobwatch_log):
+        """
+        Tries to parse beaker-jobwatch output, and looks for list of beaker
+        jobs. It then inspects these jobs, using tcms-results, to gather
+        a summary for other interested parties.
+
+        :param str jobwatch_log: Output of beaker-jobwatch.
+        :returns: tuple of three items: string result, dict with processed results, beaker matrix URL
+        """
+
+        # beaker-jobwatch output usually looks like this:
+        #
+        # Broken: 0
+        # Running:   0/1
+        # Completed: 1/1
+        # 	TJ#1739067
+        # https://beaker.engineering.redhat.com/matrix/?toggle_nacks_on=on&job_ids=1739067
+        # duration: 3:39:03.805050
+        # finished successfully
+
+        jobwatch_log = jobwatch_log.split('\n')
+
+        if len(jobwatch_log) < 3:
+            raise CIError('jobwatch output is unexpectedly short')
+
+        if not jobwatch_log[-3].startswith('https://beaker.engineering.redhat.com/matrix/'):
+            raise CIError('Don\'t know where to find beaker matrix URL in jobwatch output')
+
+        matrix_url = jobwatch_log[-3].strip()
+
+        if jobwatch_log[-1].strip() != 'finished successfully':
+            # When jobwatch failed, something serious probably happened. Give up
+            # immediately.
+            self.warn('beaker-jobwatch does not report successful completion')
+
+            return 'ERROR', {}, matrix_url
+
+        self.info('beaker-jobwatch finished successfully')
+
+        parsed_matrix_url = urlparse.urlparse(matrix_url)
+        parsed_query = urlparse.parse_qs(parsed_matrix_url.query)
+
+        TaskAggregator.WOW_MODULE = self
+
+        # tcms-results simply parses sys.argv... No other way to foist our options :/
+        old_argv, sys.argv = sys.argv, ['/bin/tcms-results', '--job={}'.format(','.join(parsed_query['job_ids']))]
+        try:
+            tcms_results.options = tcms_results.parse()
+
+        finally:
+            sys.argv = old_argv
+
+        # Now "run" tcms-results, to get a view on the results
+        self.info('tcms-results are working')
+
+        try:
+            tcms_results.Results().investigate().update()
+
+        except Exception as e:
+            raise CIError('Failed to process beaker results: {}'.format(str(e)))
+
+        processed_results = TaskAggregator.WOW_STORAGE
+
+        # Any single task with a result other than "PASS" - or even with
+        # a status other than "COMPLETED" - means testing failed.
+        #
+        # Could be written in a more compact way, using generators and all/any
+        # but this is more readable - and this part may change in the future,
+        # depending on how we redefine "passed" result.
+
+        self.debug('Try to find any non-PASS task')
+
+        for task, runs in processed_results.iteritems():
+            self.debug("    task '{}'".format(task))
+
+            for run in runs:
+                self.debug("        Status='{}', Result='{}'".format(run['bkr_status'], run['bkr_result']))
+
+                if run['bkr_status'] == 'Completed' and run['bkr_result'] == 'Pass':
+                    continue
+
+                self.debug('            We have our traitor!')
+                return 'FAIL', processed_results, matrix_url
+
+        return 'PASS', processed_results, matrix_url
 
     def execute(self):
         task = self.shared('brew_task')
@@ -52,90 +338,16 @@ class CIWow(Module):
         wow_options = _command_options('wow-options')
         jobwatch_options = _command_options('jobwatch-options')
 
-        distro_option = ['--distro={}'.format(distro)] if distro else []
-        brew_option = ['--brew-task={}'.format(task.task_id)] if task is not None else []
-
-        whiteboard = 'CI run {} brew task id {} build target {}'.format(task.nvr, task.task_id, task.target.target)
-
-        # wow
-        command = [
-            'bkr', 'workflow-tomorrow',
-            '--id',
-            '--whiteboard', whiteboard,
-            '--no-reserve'
-        ] + distro_option + brew_option + wow_options
-
-        self.info("running 'workflow-tomorrow':\n{}".format(utils.format_command_line([command])))
-
-        output = None
-        try:
-            output = run_command(command)
-
-        except CICommandError as exc:
-            # Check for most common causes, and raise soft error where necessary
-            soft = False
-
-            if 'No relevant tasks found in test plan' in exc.output.stderr:
-                soft = True
-
-            raise CIError("Failure during 'wow' execution: {}".format(exc.output.stderr), soft=soft)
+        # workflow-tomorrow
+        wow_output = self._run_wow(task, distro, wow_options)
 
         # beaker-jobwatch
-        command = [
-            'beaker-jobwatch',
-            '--skip-broken-machines'
-        ] + jobwatch_options + ['--job={}'.format(job) for job in output.stdout.split()]
+        jobwatch_output = self._run_jobwatch(wow_output.stdout.split(), jobwatch_options)
 
-        self.info("running 'beaker-jobwatch':\n{}".format(utils.format_command_line([command])))
+        # evaluate jobs
+        overall_result, processed_results, matrix_url = self._process_jobs(jobwatch_output.stdout)
 
-        command = ['bash', '-c', '{} | tee beaker-jobwatch.log'.format(' '.join(command))]
-
-        try:
-            output = run_command(command, stdout=utils.PARENT, stderr=utils.PARENT)
-
-        except CICommandError as exc:
-            raise CIError("Failure during 'jobwatch' execution: {}".format(exc.output.stderr))
-
-        with open('beaker-jobwatch.log', 'r') as f:
-            jobwatch_log = f.read().strip().split('\n')
-
-        if len(jobwatch_log) < 3:
-            raise CIError('jobwatch output is unexpectedly short')
-
-        if not jobwatch_log[-3].startswith('https://beaker.engineering.redhat.com/matrix/'):
-            raise CIError('Don\'t know where to find beaker matrix URL in jobwatch output')
-
-        matrix_url = jobwatch_log[-3].strip()
-
-        if jobwatch_log[-1].strip() == 'finished successfully':
-            self.info('beaker-jobwatch finished successfully')
-
-            parsed_matrix_url = urlparse.urlparse(matrix_url)
-            parsed_query = urlparse.parse_qs(parsed_matrix_url.query)
-
-            def _process_job(job):
-                self.debug('looking at job {}'.format(job))
-
-                output = run_command(['bkr', 'job-results', '--prettyxml', 'J:{}'.format(job)])
-
-                soup = bs4.BeautifulSoup(output.stdout, 'html.parser')
-
-                for recipe_set in soup.find_all('recipeset', attrs={'response': 'ack'}):
-                    if all((recipe['result'] == 'Pass' for recipe in recipe_set.find_all('recipe'))):
-                        continue
-
-                    return False
-
-                return True
-
-            overall_result = 'PASS' if all((_process_job(job) for job in parsed_query['job_ids'])) else 'FAIL'
-
-        else:
-            self.warn('beaker-jobwatch does not report successful completion')
-
-            overall_result = 'ERROR'
-
-        self.info('Result of wow jobs: {}'.format(overall_result))
+        self.info('Result of testing: {}'.format(overall_result))
 
         # Prepare result info
         result = {
@@ -143,12 +355,20 @@ class CIWow(Module):
             'result': overall_result,
             'urls': {
                 'beaker_matrix': matrix_url
-            }
+            },
+            'tests': processed_results
         }
 
         if 'BUILD_URL' in os.environ:
             result['urls']['jenkins_job'] = os.environ['BUILD_URL']
 
-        # Publish it
+        # Save processed beaker results as an artifact
+        with open(self.option('beaker-results'), 'w') as f:
+            f.write(format_dict(processed_results))
+            f.flush()
+
+        self.debug("Processed results saved into '{}'".format(self.option('beaker-results')))
+
+        # Publish the result
         self._results = self.shared('results') or []
         self._results.append(result)
