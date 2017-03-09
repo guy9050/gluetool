dbaff87c5b10a690059c122ae196027da88212e7
Milos Prchlik <mprchlik@redhat.com>
2017-03-09 13:08:26 +0100

    [guess-distro] Refactor target patterns out to a config file
    I checked some failed CI jobs, and it looks to me we might have to
    support more than just basic targets like "rhel-X.Y-z-candidate". Having
    all these hardcoded in the source of guess-distro will become a huge
    mess, not even mentioning "target" => "distro" is based on a
    configuration, which should lie in its own file. Therefore this patch.
    
     - makes target-autodetection method to use configuration file, listing
       target patterns and what is module supposed to do when the pattern
       matches the target. It must support ordering, it must support
       "spicing" the transofmration with BUC & nightly composes.
    
     - target-autodetect method is now just a "match pattern & call
       repl function" loop, no hardcoded targets anywhere in the code
    
     - support for batch update composes, to avoid need for errata/setup and
       similar tasks

--- libci/modules/helpers/guess_beaker_distro.py
+++ libci/modules/helpers/guess_beaker_distro.py
@@ -1,19 +1,24 @@
+import os.path
 import re
-import urllib2
 import bs4
+import yaml
 from libci import CIError, Module
-from libci.utils import log_blob, format_dict
+from libci.utils import format_dict, fetch_url, cached_property
 
 
 DEFAULT_NIGHTLY_LISTING = 'http://download.eng.brq.redhat.com/nightly/'
+DEFAULT_BU_LISTING = 'http://download-node-02.eng.bos.redhat.com/rel-eng/updates/'
 
 
 class CIGuessBeakerDistro(Module):
     """
     "Guess" distro. User can choose from different possible methods of "guessing":
 
-      - 'target-autodetection': module will transform target of brew task to a distro,
-      e.g. 'rhel-7.3-candidate' => 'rhel-7.3'. This is the default method.
+      - 'target-autodetection': module will transform target of brew task to a distro:
+        - for z-candidate targets ('rhel-7.3-candidate') will try to find corresponding
+        batch update compose ('RHEL-7.3-updates-20170222.0');
+        - for non-z-candidate targets ('rhel-7.3-candidate') will transfor taget into
+        a distro ('rhel-7.3')
 
       - 'force': use specified distro no matter what. Use --distro option to set *what*
       distro you wish to use
@@ -38,6 +43,14 @@ class CIGuessBeakerDistro(Module):
             'help': """URL where list of nightly composes lies, in a form of web server's
 directory listing. Default is {}""".format(DEFAULT_NIGHTLY_LISTING),
             'default': DEFAULT_NIGHTLY_LISTING
+        },
+        'bu-listing': {
+            'help': """URL where list of batch update composes lies, in a form of web server's
+directory listing. Default is {}""".format(DEFAULT_BU_LISTING),
+            'default': DEFAULT_BU_LISTING
+        },
+        'pattern-map': {
+            'help': 'Path to a file with target => distro patterns.'
         }
     }
 
@@ -48,6 +61,217 @@ directory listing. Default is {}""".format(DEFAULT_NIGHTLY_LISTING),
     def distro(self):
         """ return guessed distro value """
         return self._distro
+
+    @cached_property
+    def pattern_map(self):
+        """
+        Pattern map is a list of pattern: transform pairs. Pattern is a regex pattern
+        used to match the build target, transform is either a string with backreferences,
+        describing how to use groups matched by the patter to construct a distro name, or
+        it's a string of multiple items, separated by comma - in that case, the first
+        item is a string used as already described, and the second item names a function
+        that should be applied to the result of that simple replacement.
+
+        This is transformed into a list (to keep the order) of tuples (pattern, transform).
+        Pattern is compiled regex pattern. If it matches the build target, transform is
+        called, with pattern and build target as arguments. It is expected to return
+        distro name.
+        """
+
+        path = os.path.expanduser(self.option('pattern-map'))
+
+        if not os.path.exists(path):
+            raise CIError("pattern map '{}' does not exist".format(path))
+
+        with open(path, 'r') as f:
+            pattern_map = yaml.load(f)
+
+        if pattern_map is None:
+            raise CIError("pattern map '{}' does not contain any patterns".format(path))
+
+        self.debug('pattern-map:\n{}'.format(format_dict(pattern_map)))
+
+        def _create_simple_repl(repl):
+            def _replace(pattern, target):
+                """
+                Use `repl` to construct distro from `target`, honoring all backreferences made by `pattern`.
+                """
+
+                self.debug("pattern '{}', repl '{}', target '{}'".format(pattern.pattern, repl, target))
+
+                try:
+                    return pattern.sub(repl, target)
+
+                except re.error as e:
+                    raise CIError("Cannot transform pattern '{}' with target '{}', repl '{}': {}".format(
+                        pattern.pattern, target, repl, str(e)))
+
+            return _replace
+
+        def _create_buc_repl(hint_repl):
+            def _replace(pattern, target):
+                """
+                Use `hint_repl` function - which was created by `_create_simple_repl` - to get
+                a hint which is then used to find out the batch update compose.
+                """
+
+                hint = hint_repl(pattern, target)
+                self.debug("hint is '{}'".format(hint))
+
+                return self._find_buc_for_distro(hint)
+
+            return _replace
+
+        def _create_nightly_repl(hint_repl):
+            def _replace(pattern, target):
+                """
+                Use `hint_repl` function - which was created by `_create_simple_repl` - to get
+                a hint which is then used to find out the nightly compose.
+                """
+
+                hint = hint_repl(pattern, target)
+                self.debug("hint is '{}'".format(hint))
+
+                return self._find_nightly_for_distro(hint)
+
+            return _replace
+
+        transform_spice = {
+            'BUC': _create_buc_repl,
+            'NIGHTLY': _create_nightly_repl
+        }
+
+        compiled_map = []
+
+        for pattern_dict in pattern_map:
+            pattern = pattern_dict.keys()[0]
+            transform = [s.strip() for s in pattern_dict[pattern].split(',')]
+
+            # first item in `transform` is always a "repl" for `pattern.sub()` call
+            replace = _create_simple_repl(transform[0])
+
+            if len(transform) > 1:
+                # second item is another function that's using our "replace" function
+                # for its own purposes, "spicing up" the transformation process
+
+                spice = transform_spice.get(transform[1], None)
+                if spice is None:
+                    raise CIError("Unknown transform function '{}'".format(transform[1]))
+
+                replace = spice(replace)
+
+            compiled_map.append((re.compile(pattern), replace))
+
+        return compiled_map
+
+    def _get_latest_finished_compose(self, base_url, hint):
+        """
+        Fetch index page listing several composes from BASE_URL, and try to find
+        the most recent and FINISHED one, using HINT to limit set of examined
+        composes - if the composes starts with HINT, we'll check it.
+
+        :param str base_url: URL of the index page. It should be a directory listing,
+          with links leading to relevant composes.
+        :param str hint: what composs should be examined - it the name of compose
+          starts with `hint`, it's one of ours.
+        :returns: name of the compose, or `None`.
+        """
+
+        # Fetch the index
+        try:
+            _, content = fetch_url(base_url, logger=self.logger)
+
+        except CIError:
+            raise CIError('Cannot get list of available composes')
+
+        # Find all <a/> elements from the index
+        soup = bs4.BeautifulSoup(content, 'html.parser')
+
+        # [(text, href), ...]
+        composes = [(link.string.replace('/', ''), link['href'])
+                    for link in soup.find_all('a') if link.string.startswith(hint)]
+
+        self.debug('available composes:\n{}'.format(format_dict(composes)))
+
+        for name, href in sorted(composes, key=lambda x: x[0], reverse=True):
+            self.debug("checking status of '{}'".format(name))
+
+            # Check compose status
+            url = '{}/{}/STATUS'.format(base_url, href)
+
+            try:
+                _, content = fetch_url(url, logger=self.logger)
+
+            except CIError:
+                self.warn("Cannot find out status of '{}'".format(name))
+                continue
+
+            if content.strip() != 'FINISHED':
+                self.debug("'{}' not marked as finished".format(name))
+                continue
+
+            # Get its ID
+            url = '{}/{}/COMPOSE_ID'.format(base_url, href)
+
+            try:
+                _, content = fetch_url(url, logger=self.logger)
+
+            except CIError:
+                self.warn("Cannot find out ID of '{}'".format(name))
+                continue
+
+            return content.strip()
+
+        return None
+
+    def _find_buc_for_distro(self, hint):
+        """
+        Find batch update compose for a given distro.
+
+        :param str hint: Values like "RHEL-7.3", "RHEL-6.8", etc.
+        :returns: BU compose name.
+        """
+
+        hint = hint.upper()
+
+        self.debug("Looking for latest valid BU compose for '{}'".format(hint))
+
+        # First, try to take "latest-FOO" shortcut
+        url = self.option('bu-listing') + '/latest-{}'.format(hint) + '/COMPOSE_ID'
+
+        try:
+            _, content = fetch_url(url, logger=self.logger)
+            return content.strip()
+
+        except CIError:
+            self.warn("Cannot find shortcut '/latest-{}'".format(hint))
+
+        # Ok, so there's no "latest-FOO" directory, lets iterate over all available composes
+        distro = self._get_latest_finished_compose('{}/{}'.format(self.option('bu-listing'), hint), hint)
+
+        if distro is None:
+            raise CIError('None of examined BU composes was acceptable')
+
+        return distro
+
+    def _find_nightly_for_distro(self, hint):
+        """
+        Find nightly compose for a give distro.
+
+        :param str hint: Values like "RHEL-7.3", "RHEL-6.8", etc.
+        :returns: Nightly compose name.
+        """
+
+        hint = hint.upper()
+
+        self.debug("Looking for latest valid nightly compose for '{}'".format(hint))
+
+        distro = self._get_latest_finished_compose(self.option('nightly-listing'), hint)
+
+        if distro is None:
+            raise CIError('None of examined nightly composes was acceptable')
+
+        return distro
 
     def _guess_force(self):
         distro = self.option('distro')
@@ -60,66 +284,27 @@ directory listing. Default is {}""".format(DEFAULT_NIGHTLY_LISTING),
         if task is None:
             raise CIError("Using 'target-autodetect' method without a brew task does not work")
 
-        translations = {
-            # default translation for rhel and staging branches
-            r'(staging-|supp-)?(rhel-[0-9]+)(.[0-9]+)?-?(z)?-candidate':
-                lambda match: '{}{}{}'.format(match.group(2),
-                                              match.group(3) or '',
-                                              '.' + match.group(4) if match.group(4) else ''),
-            # RHEL LE 7.1 product
-            r'rhel-7.1-ppc64le-?(z)?-candidate':
-                lambda match: 'rhel-le-7.1{}'.format('.' + match.group(1) if match.group(1) else '')
-        }
+        target = task.target.target
 
-        for regex, function in translations.items():
-            match = re.match(regex, task.target.target)
-            if match:
-                self._distro = function(match)
-                break
-        else:
-            raise CIError("could not translate build target '{}' to distro".format(task.target.target))
+        self.debug("trying to match target '{}'".format(target))
 
-        self.debug("transformed target '{}' to distro '{}'".format(task.target.target, self._distro))
+        for pattern, transform in self.pattern_map:
+            self.debug("testing pattern '{}'".format(pattern.pattern))
 
-    def _guess_nightly(self):
-        distro = self.option('distro')
-        url = self.option('nightly-listing')
-
-        self.debug("Fetching list of nightly composes from '{}'".format(url))
-
-        response = urllib2.urlopen(url)
-        code, content = response.getcode(), response.read()
-
-        log_blob(self.debug, '{}: {}'.format(url, code), content)
-
-        if code != 200:
-            raise CIError('Cannot get list of nightly composes')
-
-        soup = bs4.BeautifulSoup(content, 'html.parser')
-
-        nightlies = [(link.string.replace('/', ''), link['href'])
-                     for link in soup.find_all('a') if link.string.startswith(distro)]
-
-        self.debug('available nightlies:\n{}'.format(format_dict(nightlies)))
-
-        for name, href in sorted(nightlies, key=lambda x: x[0], reverse=True):
-            self.debug("Checking status of '{}'".format(name))
-
-            status_url = url + '/' + href + '/STATUS'
-            response = urllib2.urlopen(status_url)
-            code, content = response.getcode(), response.read()
-
-            log_blob(self.debug, '{}: {}'.format(status_url, code), content)
-
-            if content.strip() != 'FINISHED':
-                self.debug('{} not marked as finished'.format(name))
+            match = pattern.match(target)
+            if match is None:
                 continue
 
-            self._distro = name
+            self.debug('  matched')
+
+            self._distro = transform(pattern, target)
             break
 
         else:
-            raise CIError('None of examined nightlies was marked as FINISHED')
+            raise CIError("could not translate build target '{}' to distro".format(target))
+
+    def _guess_nightly(self):
+        self._distro = self._find_nightly_for_distro(self.option('distro'))
 
     _methods = {
         'force': _guess_force,
@@ -129,10 +314,13 @@ directory listing. Default is {}""".format(DEFAULT_NIGHTLY_LISTING),
 
     def sanity(self):
         distro_required = ('force', 'nightly')
-        distro_ignored = ('target-autodetection')
+        distro_ignored = ('target-autodetection',)
 
         method = self.option('method')
         distro = self.option('distro')
+
+        if method == 'target-autodetection' and not self.option('pattern-map'):
+            raise CIError("--pattern-map option is required with method '{}'".format(method), soft=True)
 
         if method in distro_required and not distro:
             raise CIError("--distro option is required with method '{}'".format(method), soft=True)
