f1aea09c18e3b4307d65ec0b436bf3c03779f115
Milos Prchlik <mprchlik@redhat.com>
2017-03-15 17:07:49 +0100

    Restraint-based testing

---
+++ libci/modules/testing/restraint/runner.py
@@ -0,0 +1,226 @@
+import copy
+import tempfile
+import bs4
+
+import libci
+
+
+DEFAULT_RESTRAINT_PORT = 8081
+
+
+class RestraintRunner(libci.Module):
+    name = 'restraint-runner'
+
+    options = {
+        'use-snapshots': {
+            'help': 'Enable or disable use of snapshots (if supported by guests) (default: no)',
+            'default': 'no',
+            'metavar': 'yes|no'
+        },
+        'parallelize': {
+            'help': 'Enabel or disable parralelization of recipe sets and task sets (default: no)',
+            'default': 'no',
+            'metavar': 'yes|no'
+        }
+    }
+
+    def _bool_option(self, name):
+        value = self.option(name)
+        if value is None:
+            return False
+
+        return True if value.strip().lower() == 'yes' else False
+
+    @libci.utils.cached_property
+    def use_snapshots(self):
+        return self._bool_option('use-snapshots')
+
+    @libci.utils.cached_property
+    def parallelize(self):
+        return self._bool_option('parallelize')
+
+    def _guest_restraint_address(self, guest):
+        return '{}:{}/{}'.format(guest.hostname, DEFAULT_RESTRAINT_PORT, guest.port)
+
+    def _run_task_set(self, guest, task_set, recipe_attrs, recipe_set_attrs):
+        """
+        Run a set of tasks on the guest.
+
+        :param Guest guest: guest to use for running tests.
+        :param task_set: list of <task/> elements, representing separate tasks.
+        :param dict recipe_attrs: additional attributes to set on <recipe/> element.
+        :param dict recipe_set_attrs: additional attributes to set on <recipe_set/> element.
+        """
+
+        soup = bs4.BeautifulSoup('', 'xml')
+
+        # Log our task set
+        guest.debug('Task set:\n{}'.format('\n'.join([task.prettify() for task in task_set])))
+
+        # Wrap task set in <job><recipeSet><recipe>... envelope
+        job = soup.new_tag('job')
+        job.append(soup.new_tag('recipeSet', **recipe_set_attrs))
+        job.recipeSet.append(soup.new_tag('recipe', **recipe_attrs))
+
+        for task in task_set:
+            job.recipeSet.recipe.append(copy.copy(task))
+
+        # We'll need this for restraint
+        job_desc = job.prettify()
+
+        self.debug('Job:\n{}'.format(job_desc))
+
+        # Write out our job description, and tell restraint to run it
+        with tempfile.NamedTemporaryFile() as f:
+            f.write(job_desc)
+            f.flush()
+
+            try:
+                output = libci.utils.run_command([
+                    '/usr/bin/restraint', '-v',
+                    '--host', '1={}@{}'.format(guest.username, self._guest_restraint_address(guest)),
+                    '--job', f.name
+                ], logger=guest.logger)
+
+            except libci.CICommandError as e:
+                if e.output.stderr.strip().startswith('One or more tasks failed'):
+                    pass
+
+                else:
+                    raise
+
+        self.info('Task set output:\n{}'.format(output.stdout))
+
+        return ('task set result', )  # results
+
+    def _run_recipe_set_isolated(self, guest, recipe_set):
+        """
+        Run tasks from a recipe set one by one, getting fresh snapshot for each task.
+
+        :param element recipe_set: <recipeSet/> element, gathering some tasks.
+        """
+
+        guest.info('Running recipe set tasks in isolation')
+
+        # _run_task_set will need these, to make tasks feel like home
+        recipe_set_attrs = recipe_set.attrs
+        recipe_attrs = recipe_set.find_all('recipe')[0].attrs
+
+        tasks = recipe_set.find_all('task')
+
+        # if it's just a single task, it's quite simple
+        if len(tasks) == 1:
+            self.debug('only a single task in the task set, use guest directly')
+
+            return self._run_task_set(guest, tasks, recipe_attrs, recipe_set_attrs)
+
+        # save current state of guest
+        base_snapshot = guest.create_snapshot()
+
+        if self.parallelize:
+            # run all task in parallel, each on its own guest, using the snapshot as their image
+            self.info('Running {} tasks in parallel'.format(len(tasks)))
+            self.debug('parallelize {} tasks requires {} additional guests'.format(len(tasks), len(tasks) - 1))
+
+            guests = [guest] + self.shared('openstack_provision', len(tasks) - 1, image=base_snapshot)
+            threads = []
+
+            for i, (guest, task) in enumerate(zip(guests, tasks)):
+                thread = libci.utils.WorkerThread(guest.logger, self._run_task_set, fn_args=(guest, [task], recipe_attrs, recipe_set_attrs), name='task-runner-{}'.format(i))
+                threads.append(thread)
+
+                thread.start()
+
+            self.debug('wait for all worker threads to finish')
+            for thread in threads:
+                thread.join()
+
+            results = [thread.result for thread in threads]
+
+#            if any((isinstance(thread.result, Exception) for thread in setup_threads)):
+#                self.error('At least one guest setup failed')
+#                self.error('Note: see detailed exception in debug log for more information')
+#                raise CIError('At least one guest setup failed')
+
+        else:
+            # run all tasks one by one, on the same guest, restoring the snapshot between tasks
+            self.info('Running {} tasks one by one'.format(len(tasks)))
+
+            results = []
+
+            for task in recipe_set.find_all('task'):
+                guest.debug("restoring snapshot '{}' before running next task".format(base_snapshot))
+                actual_guest = guest.restore_snapshot(base_snapshot)
+
+                results.append(self._run_task_set(actual_guest, [task], recipe_attrs, recipe_set_attrs))
+
+        return results
+
+    def _run_recipe_set_whole(self, guest, recipe_set):
+        """
+        Run tasks from a recipe set in a "classic" manner, runnign one by one
+        on the same box.
+
+        :param element recipe_set: <recipeSet/> element, gathering some tasks.
+        """
+
+        guest.info('Running recipe set tasks in the same environment, one by one')
+
+        return self._run_task_set(guest, recipe_set.find_all('task'),
+                                  recipe_set.find_all('recipe')[0].attrs, recipe_set.attrs)
+
+    def _run_recipe_set(self, guest, recipe_set):
+        """
+        Run recipe set on a given guest.
+
+        :param Guest guest: guest we use for our tests.
+        :param element recipe_set: <recipeSet/> element, grouping tasks.
+        """
+
+        # this makes situation easier - I decided to limit number of <recipe/>
+        # elements inside <recipeSet/> to exactly one. I don't know what options
+        # would make wow to create more recipes inside recipeSet, and I want
+        # to find out, but for the proof of concept, this makes my living easy
+        # to bear.
+        assert len(recipe_set.find_all('recipe')) == 1
+
+        guest.info('Running recipe set:\n{}'.format(recipe_set.prettify()))
+
+        if guest.supports_snapshots() is True and self.use_snapshots:
+            results = self._run_recipe_set_isolated(guest, recipe_set)
+
+        else:
+            results = self._run_recipe_set_whole(guest, recipe_set)
+
+        guest.info('Recipe set finished!')
+        return results
+
+    def execute(self):
+        schedule = self.shared('schedule') or []
+
+        if self.parallelize:
+            self.info('Scheduled {} items, running them in parallel'.format(len(schedule)))
+
+            threads = []
+
+            for i, (guest, recipe_set) in enumerate(schedule):
+                thread = libci.utils.WorkerThread(self.logger, self._run_recipe_set, fn_args=(guest, recipe_set), name='recipe-set-runner-{}'.format(i))
+                threads.append(thread)
+
+                thread.start()
+
+            self.debug('wait for all recipe set threads to finish')
+            for thread in threads:
+                thread.join()
+
+            results = [thread.result for thread in threads]
+
+        else:
+            self.info('Scheduled {} items, running them one by one'.format(len(schedule)))
+
+            results = [self._run_recipe_set(guest, recipe_set) for guest, recipe_set in schedule]
+
+        self.info('Recipe sets results:\n{}'.format(results))
+
+    def destroy(self, failure=None):
+        pass
---
+++ libci/modules/testing/restraint/scheduler.py
@@ -0,0 +1,204 @@
+import shlex
+import bs4
+
+import libci
+from libci import utils, CIError, CICommandError
+
+
+class RestraintScheduler(libci.Module):
+    name = 'restraint-scheduler'
+
+    options = {
+        'wow-options': {
+            'help': 'Additional options for workflow-tomorrow'
+        }
+    }
+
+    shared_functions = ['schedule']
+
+    _schedule = None
+
+    def schedule(self):
+        return self._schedule
+
+    def _run_wow(self, task, distro, options):
+        """
+        Run workflow-tomorrow to create beaker job description, using options we
+        got from the user.
+
+        :param task: brew task info, as returned by `brew_task` shared function
+        :param str distro: distribution to install.
+        :param list options: additional options, usualy coming from wow-options option.
+        :returns: libci.utils.ProcessOutput with the output of w-t.
+        """
+
+        self.info('running workflow-tomorrow to get job description')
+
+        distro_option = ['--distro={}'.format(distro)] if distro else []
+
+        # wow
+        task_params = {
+            'BASEOS_CI': 'true',
+            'BASEOS_CI_COMPONENT': str(task.component)
+        }
+
+        command = [
+            'bkr', 'workflow-tomorrow',
+            '--dry',  # this will make wow to print job description in XML
+            '--single',  # ignore multihost tests
+            '--no-reserve',  # don't reserve hosts
+            '--decision',  # show desicions about including/not including task in the job
+            '--hardware-skip',  # ignore tasks with specific hardware requirements
+            '--arch', 'x86_64',  # limit to x86_64, we're dealing with openstack - I know :(
+            '--restraint',
+            '--suppress-install-task'
+        ] + distro_option + options
+
+        for name, value in task_params.iteritems():
+            command += ['--taskparam', '{}={}'.format(name, value)]
+
+        try:
+            return libci.utils.run_command(command)
+
+        except CICommandError as exc:
+            # Check for most common causes, and raise soft error where necessary
+            soft = False
+
+            if 'No relevant tasks found in test plan' in exc.output.stderr:
+                soft = True
+
+            raise CIError("Failure during 'wow' execution: {}".format(exc.output.stderr), soft=soft)
+
+    def _setup_guest(self, task_id, guest):
+        # pylint: disable=no-self-use
+
+        """
+        Run necessary command to prepare guest for following procedures.
+        """
+
+        guest.debug('setting the guest up')
+
+        # additional repositories
+        repos = {
+            'beaker-harness': (
+                'http://beaker.engineering.redhat.com/harness/RedHatEnterpriseLinux7/',
+                {'gpgcheck': 0, 'enabled': 0}
+            ),
+            'restraint': (
+                'http://file.bos.redhat.com/~bpeck/restraint/rhel7--test/x86_64/',
+                {'gpgcheck': 0, 'enabled': 0}
+            ),
+            'beaker-tasks': (
+                'http://beaker.engineering.redhat.com/rpms/',
+                {'gpgcheck': 0, 'enabled': 1}
+            )
+        }
+
+        for name, (baseurl, options) in repos.iteritems():
+            guest.debug("adding '{}' repository ({}, {})".format(name, baseurl, options))
+
+            guest.create_repo(name, name, baseurl, **options)
+
+        guest.execute('yum clean metadata')
+
+        # install restraint & other harness-related packages
+        guest.execute('yum install -y --enablerepo=restraint --enablerepo=beaker-harness beakerlib beakerlib-redhat restraint psmisc yum-plugin-priorities parted createrepo')
+        guest.execute('chkconfig --level 345 restraintd on')
+        guest.execute('service restraintd start')
+
+        # install brew build
+        guest.debug('installing brew task packages')
+        guest.execute('yum install -y --enablerepo=restraint --enablerepo=beaker-tasks distribution-distribution-install-brew-build.noarch')
+        guest.execute('METHOD=install TASKS={} make -C /mnt/tests/distribution/install/brew-build/'.format(task_id))
+
+    def create_schedule(self, task, job_desc):
+        """
+        Main workhorse - given the job XML, get some guests, and create pairs
+        (guest, tasks) for runner to process.
+        """
+
+        libci.utils.log_blob(self.debug, 'full job description', job_desc.prettify())
+
+        self._schedule = []
+
+        recipe_sets = job_desc.find_all('recipeSet')
+
+        self.info('job contains {} recipe sets, asking for guests'.format(len(recipe_sets)))
+
+        # get corresponding number of guests
+        guests = self.shared('openstack_provision', len(recipe_sets), image='rhel-7.3-server-x86_64-updated')
+
+        assert guests is not None
+        assert len(guests) == len(recipe_sets)
+
+        # there are tags that make not much sense for restraint - we'll filter them out
+        def _remove_tags(recipe_set, name):
+            self.debug("removing tags '{}'".format(name))
+
+            for tag in recipe_set.find_all(name):
+                tag.decompose()
+
+        setup_threads = []
+
+        for guest, recipe_set in zip(guests, recipe_sets):
+            self.debug('guest: {}'.format(str(guest)))
+            self.debug('recipe set:\n{}'.format(recipe_set.prettify()))
+
+            # remove tags we want to filter out
+            for tag in ('distroRequires', 'hostRequires', 'repos', 'partitions'):
+                _remove_tags(recipe_set, tag)
+
+            self.debug('final recipe set:\n{}'.format(recipe_set.prettify()))
+
+            self._schedule.append((guest, recipe_set))
+
+            # setup guest
+            thread_name = 'setup-guest-{}'.format(guest.name)
+            thread = utils.WorkerThread(guest.logger,
+                                        self._setup_guest, fn_args=(task.task_id, guest,),
+                                        name=thread_name)
+            setup_threads.append(thread)
+
+            thread.start()
+            self.debug("setup thread '{}' started".format(thread_name))
+
+        self.info('waiting for all guests to finish their initial setup')
+        for thread in setup_threads:
+            thread.join()
+
+        if any((isinstance(thread.result, Exception) for thread in setup_threads)):
+            self.error('At least one guest setup failed')
+            self.error('Note: see detailed exception in debug log for more information')
+
+            raise CIError('At least one guest setup failed')
+
+        self.debug('Schedule:')
+        for guest, recipe_set in self._schedule:
+            libci.utils.log_blob(self.debug, str(guest), recipe_set.prettify())
+
+    def execute(self):
+        task = self.shared('brew_task')
+        if task is None:
+            raise CIError('no brew build found, did you run brew module')
+
+        distro = self.shared('distro')
+
+        def _command_options(name):
+            opts = self.option(name)
+            if opts is None or not opts:
+                return []
+
+            return shlex.split(opts)
+
+        wow_options = _command_options('wow-options')
+
+        # workflow-tomorrow
+        wow_output = self._run_wow(task, distro, wow_options)
+
+        job = bs4.BeautifulSoup(wow_output.stdout, 'xml')
+        self.debug('job as planned by wow:\n{}'.format(job.prettify()))
+
+        self.create_schedule(task, job)
+
+    def destroy(self, failure=None):
+        pass
