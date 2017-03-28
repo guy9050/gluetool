2e7dba311564b26b69fb6464c9cfb000e47c0eec
Milos Prchlik <mprchlik@redhat.com>
2017-03-28 09:59:47 +0200

    Ansible helper module - play playbooks

---
+++ libci/modules/helpers/ansible.py
@@ -0,0 +1,58 @@
+import json
+
+import libci
+
+
+class Ansible(libci.Module):
+    """
+    Helper module - give it a playbook, a host, maybe few additional variables,
+    and let Ansible perform it.
+    """
+
+    name = 'ansible'
+
+    shared_functions = ('run_playbook',)
+
+    def run_playbook(self, playbook_path, hosts, variables=None):
+        """
+        Run Ansible playbook.
+
+        :param str playbook_path: Path to the playbook.
+        :param [str, ...] hosts: Specifications of hosts, forming Ansible inventory.
+        :param dict variables: If set, represents additional variables that will
+          be passed to ``ansible-playbook`` using ``--extra-vars`` option.
+        :returns: :py:class:`libci.utils.ProcessOutput` instance.
+        """
+
+        cmd = [
+            'ansible-playbook',
+            '-i', '{},'.format(','.join(hosts))  # note the comma
+        ]
+
+        if variables:
+            cmd += [
+                '--extra-vars',
+                ' '.join(['{}="{}"'.format(k, v) for k, v in variables.iteritems()])
+            ]
+
+        cmd += [playbook_path]
+
+        try:
+            return libci.utils.run_command(cmd)
+
+        except libci.CICommandError as e:
+            self.error('Failure during ansible playbook execution')
+
+            for line in e.output.stdout.split('\n'):
+                line = line.strip()
+
+                if not line.startswith('fatal: '):
+                    continue
+
+                message = json.loads(line[line.index('{'):])
+                self.error('Ansible says: {}'.format(message['msg']))
+
+            raise libci.CIError('Failure during Ansible playbook execution. See log for details.')
+
+    def execute(self):
+        pass
