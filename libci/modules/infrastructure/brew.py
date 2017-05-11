5fd2283457bc2550432898d747beabce5e8edd75
Ondrej Ptak <optak@redhat.com>
2017-05-11 17:45:53 +0200

    BrewTask.issuer:substitute jenkins with real user

--- libci/modules/infrastructure/brew.py
+++ libci/modules/infrastructure/brew.py
@@ -2,10 +2,13 @@ import re
 import koji
 import libci
 from libci import CIError, SoftCIError
-from libci.utils import cached_property, format_dict
+from libci.utils import cached_property, format_dict, fetch_url
 
 BREW_API_TOPURL = "http://download.eng.bos.redhat.com/brewroot"
 BREW_WEB_URL = 'https://brewweb.engineering.redhat.com/brew/'
+#AUTOMATION_USER_ID =  2863 # jenkins
+AUTOMATION_USER_ID =  2522 # optak
+GIT_COMMIT_URL = 'http://pkgs.devel.redhat.com/cgit/rpms/{0}/commit/?id={1}' # component, hash
 
 
 class NoArtifactsError(SoftCIError):
@@ -54,6 +57,25 @@ class BrewTask(object):
     def owner(self):
         owner_id = self.task_info["owner"]
         return self.brew.getUser(owner_id)["name"]
+
+    @cached_property
+    def issuer(self):
+        owner_id = self.task_info["owner"]
+        if owner_id == AUTOMATION_USER_ID:
+            self._module.info("Automation user detected, need to get git commit issuer")
+            # get git commit hash and component name
+            request = self.task_info["request"][0]
+            git_hash = re.search("#[^']*", request).group()[1:]
+            component = re.search("/rpms/[^?]*", request).group()[6:]
+            # get git commit html
+            url_info, commit_html = fetch_url(GIT_COMMIT_URL.format(component, git_hash))
+            issuer = re.search("committer.*</td>", commit_html).group()
+            issuer = re.sub(".*lt;(.*)@.*", "\\1", issuer)
+            self._module.info("Git commit issuer: {0}".format(issuer))
+            return issuer
+        else:
+            name = self.brew.getUser(owner_id)["name"]
+        return name
 
     @cached_property
     def target(self):
