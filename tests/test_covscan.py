47c9340f53c64cab3af0ac2b623937b642377af6
Martin Kluson <mkluson@redhat.com>
2017-08-02 15:32:55 +0200

    Refactoring of code according to comments from code review

--- tests/test_covscan.py
+++ tests/test_covscan.py
@@ -46,93 +46,67 @@ def fixture_module():
     return create_module(CICovscan)
 
 
+def test_loadable(module):
+    ci, _ = module
+    # pylint: disable=protected-access
+    python_mod = ci._load_python_module('static_analysis/covscan', 'pytest_covscan',
+                                        'libci/modules/static_analysis/covscan/covscan.py')
+
+    assert hasattr(python_mod, 'CICovscan')
+
+
 def test_no_brew(module, monkeypatch):
-
-    def mocked_shared(key):
-        return {
-            'brew_task': None
-        }[key]
-
     _, module = module
 
-    monkeypatch.setattr(module, 'shared', mocked_shared)
+    monkeypatch.setattr(module, 'shared', MagicMock(return_value=None))
 
     with pytest.raises(libci.CIError, match=r'^no brew build found, did you run brew module?'):
         module.execute()
 
 
-def test_blacklisted_component(module, monkeypatch):
+def test_blacklisted_component(log, module, monkeypatch):
     component_name = 'kernel'
-    blacklist = component_name + ',libreoffice'
-
-    mocked_task = MagicMock(component=component_name)
-    mocked_info = MagicMock()
-
-    def mocked_shared(key):
-        return {
-            'brew_task': mocked_task
-        }[key]
-
-    def mocked_option(key):
-        return {
-            'blacklist': blacklist
-        }[key]
 
     _, module = module
+    module._config['blacklist'] = '{},libreoffice'.format(component_name)
 
-    monkeypatch.setattr(module, 'shared', mocked_shared)
-    monkeypatch.setattr(module, 'option', mocked_option)
-    monkeypatch.setattr(module, 'info', mocked_info)
+    monkeypatch.setattr(module, 'shared', MagicMock(return_value=MagicMock(component=component_name)))
 
     module.execute()
 
-    mocked_info.assert_called_with('Skipping blacklisted package {}'.format(component_name))
+    assert log.records[-1].message == 'Skipping blacklisted package {}'.format(component_name)
 
 
-def test_not_enabled_target(module, monkeypatch):
+def test_not_enabled_target(log, module, monkeypatch):
     enabled_target = '(rhel|RHEL)-[67].[0-9]+(-z)?-candidate,rhel-7.1-ppc64le(-z)?-candidate'
     component_name = 'ssh'
     target = 'not_allowed'
 
-    mocked_target = MagicMock(target=target)
-    mocked_task = MagicMock(target=mocked_target, component=component_name)
-    mocked_info = MagicMock()
-    mocked_scan = MagicMock()
-
-    def mocked_shared(key):
-        return {
-            'brew_task': mocked_task
-        }[key]
-
-    def mocked_option(key):
-        return {
-            'blacklist': None,
-            'target_pattern': enabled_target
-        }[key]
+    mocked_task = MagicMock(target=MagicMock(target=target), component=component_name)
 
     _, module = module
+    module._config['target_pattern'] = enabled_target
 
-    monkeypatch.setattr(module, 'shared', mocked_shared)
-    monkeypatch.setattr(module, 'option', mocked_option)
-    monkeypatch.setattr(module, 'info', mocked_info)
-    monkeypatch.setattr(module, 'scan', mocked_scan)
+    monkeypatch.setattr(module, 'shared', MagicMock(return_value=mocked_task))
 
     module.execute()
 
-    mocked_info.assert_called_with('Target {} is not enabled, skipping job'.format(target))
+    assert log.records[-1].message == 'Target {} is not enabled, skipping job'.format(target)
 
 
-def run(result, module, monkeypatch, tmpdir):
+def run(result, log, module, monkeypatch, tmpdir):
     enabled_target = '(rhel|RHEL)-[67].[0-9]+(-z)?-candidate,rhel-7.1-ppc64le(-z)?-candidate'
     component_name = 'ssh'
     target = 'rhel-7.4-candidate'
 
     mocked_target = MagicMock(target=target)
-    mocked_task = MagicMock(target=mocked_target, component=component_name)
-    mocked_info = MagicMock()
+    mocked_task = MagicMock(target=mocked_target, component=component_name, srcrpm='dummy.src.rpm')
+
+    _, module = module
+    module._config['target_pattern'] = enabled_target
 
     def mocked_urlopen(url):
-        if (url.find('added') or url.find('fixed')) != -1:
+        if 'added' in url or 'fixed' in url:
             file_name = 'dummy_file.html'
             outfile = tmpdir.join(file_name)
 
@@ -142,12 +116,11 @@ def run(result, module, monkeypatch, tmpdir):
                 outfile.write(ADDED_FAIL)
             return outfile
 
-        elif (url.find('stdout')) != -1:
-            file_name = str(tmpdir) + 'dummy_file.log.gz'
-            content = b'Lots of content here'
-            with gzip.open(file_name, 'wb') as outfile:
-                outfile.write(content)
-            return open(file_name, 'r')
+        elif 'stdout' in url:
+            outfile = tmpdir.join('dummy_file.log.gz')
+            with gzip.open(str(outfile), 'wb') as f:
+                f.write(b'Lots of content here')
+            return outfile
 
         else:
             return ''
@@ -156,12 +129,11 @@ def run(result, module, monkeypatch, tmpdir):
         with open(cmd[-1], 'w') as outfile:
             outfile.write('1234')
 
-    def mocked_grabber(cmd):
-        if cmd.find('rpm') != -1:
-            file_name = 'dummy_file.rpm'
-            with open(file_name, 'w') as outfile:
+    def mocked_grabber(url):
+        if 'rpm' in url:
+            with open(url, 'w') as outfile:
                 outfile.write('')
-            return os.path.abspath(file_name)
+            return os.path.abspath(url)
         else:
             pass
 
@@ -170,33 +142,22 @@ def run(result, module, monkeypatch, tmpdir):
             'brew_task': mocked_task
         }[key]
 
-    def mocked_option(key):
-        return {
-            'blacklist': None,
-            'target_pattern': enabled_target,
-            'task-id': None
-        }[key]
-
-    _, module = module
-
     monkeypatch.setattr(module, 'shared', mocked_shared)
-    monkeypatch.setattr(module, 'option', mocked_option)
-    monkeypatch.setattr(module, 'info', mocked_info)
     monkeypatch.setattr(libci.modules.static_analysis.covscan.covscan, 'urlgrab', mocked_grabber)
     monkeypatch.setattr(libci.modules.static_analysis.covscan.covscan, 'run_command', mocked_run_command)
     monkeypatch.setattr(libci.modules.static_analysis.covscan.covscan, 'urlopen', mocked_urlopen)
 
     module.execute()
 
-    mocked_info.assert_called_with('Result of testing: {}'.format(result))
+    assert log.records[-2].message == 'Result of testing: {}'.format(result)
 
 
-def test_pass_run(module, monkeypatch, tmpdir):
-    run('PASSED', module, monkeypatch, tmpdir)
+def test_pass_run(log, module, monkeypatch, tmpdir):
+    run('PASSED', log, module, monkeypatch, tmpdir)
 
 
-def test_fail_run(module, monkeypatch, tmpdir):
-    run('FAILED', module, monkeypatch, tmpdir)
+def test_fail_run(log, module, monkeypatch, tmpdir):
+    run('FAILED', log, module, monkeypatch, tmpdir)
 
 
 def test_run_command_error(module, monkeypatch):
@@ -230,15 +191,8 @@ def test_invalid_json(monkeypatch, tmpdir):
         result.added
 
 
-def test_no_baseline(module, monkeypatch):
+def test_no_baseline(module):
     _, module = module
-
-    def mocked_option(key):
-        return {
-            'task-id': None
-        }[key]
-
-    monkeypatch.setattr(module, 'option', mocked_option)
 
     module.brew_task = MagicMock(latest=None)
 
@@ -278,19 +232,12 @@ def test_fetch_fixed(monkeypatch, tmpdir):
 def test_covscan_fail(module, monkeypatch, tmpdir):
     _, module = module
 
-    def mocked_option(key):
-        return {
-            'task-id': None
-        }[key]
-
     def mocked_grabber(cmd):
-        if cmd.find('rpm') != -1:
-            file_name = 'dummy_file.rpm'
-            with open(file_name, 'w') as outfile:
-                outfile.write('')
-            return os.path.abspath(file_name)
-        else:
-            pass
+        # pylint: disable=unused-argument
+        file_name = 'dummy_file.rpm'
+        with open(file_name, 'w') as outfile:
+            outfile.write('')
+        return os.path.abspath(file_name)
 
     def mocked_run_command(cmd):
         with open(cmd[-1], 'w') as outfile:
@@ -298,14 +245,11 @@ def test_covscan_fail(module, monkeypatch, tmpdir):
 
     def mocked_urlopen(url):
         # pylint: disable=unused-argument
-        file_name = str(tmpdir) + 'dummy_file.log.gz'
-        content = b"Failing because of at least one subtask hasn't closed properly.\n"
-        with gzip.open(file_name, 'wb') as outfile:
-            outfile.write(content)
+        outfile = tmpdir.join('dummy_file.log.gz')
+        with gzip.open(str(outfile), 'wb') as f:
+            f.write(b"Failing because of at least one subtask hasn't closed properly.\n")
+        return outfile
 
-        return open(file_name, 'r')
-
-    monkeypatch.setattr(module, 'option', mocked_option)
     monkeypatch.setattr(libci.modules.static_analysis.covscan.covscan, 'urlgrab', mocked_grabber)
     monkeypatch.setattr(libci.modules.static_analysis.covscan.covscan, 'run_command', mocked_run_command)
     monkeypatch.setattr(libci.modules.static_analysis.covscan.covscan, 'urlopen', mocked_urlopen)
@@ -317,39 +261,31 @@ def test_covscan_fail(module, monkeypatch, tmpdir):
         module.scan()
 
 
-def test_setted_taskid(module, monkeypatch, tmpdir):
+def test_setted_taskid(log, module, monkeypatch, tmpdir):
     _, module = module
-
-    def mocked_option(key):
-        return {
-            'task-id': '1234'
-        }[key]
+    module._config['task-id'] = '1234'
 
     def mocked_urlopen(url):
-        if (url.find('added') or url.find('fixed')) != -1:
+        if 'added' in url or 'fixed' in url:
             file_name = 'dummy_file.html'
             outfile = tmpdir.join(file_name)
             outfile.write(ADDED_PASS)
             return outfile
 
-        elif (url.find('stdout')) != -1:
-            file_name = str(tmpdir) + 'dummy_file.log.gz'
-            content = b'Lots of content here'
-            with gzip.open(file_name, 'wb') as outfile:
-                outfile.write(content)
-            return open(file_name, 'r')
+        elif 'stdout' in url:
+            outfile = tmpdir.join('dummy_file.log.gz')
+            with gzip.open(str(outfile), 'wb') as f:
+                f.write(b'Lots of content here')
+            return outfile
 
     def mocked_grabber(cmd):
         # pylint: disable=unused-argument
         pass
 
-    mocked_info = MagicMock()
-
-    monkeypatch.setattr(module, 'option', mocked_option)
-    monkeypatch.setattr(module, 'info', mocked_info)
     monkeypatch.setattr(libci.modules.static_analysis.covscan.covscan, 'urlopen', mocked_urlopen)
     monkeypatch.setattr(libci.modules.static_analysis.covscan.covscan, 'urlgrab', mocked_grabber)
 
     module.scan()
 
-    mocked_info.assert_any_call('Skipping covscan testing, using existing Covscan task id')
+    message = 'Skipping covscan testing, using existing Covscan task id'
+    assert any(record.message == message for record in log.records)
