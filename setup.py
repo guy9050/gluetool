e0336392c0719b9c7766e6e70926e5ac323cf99e
Milos Prchlik <mprchlik@redhat.com>
2018-03-05 19:15:13 +0100

    setup: generate list of dynamic files instead of hardcoding it
    We forgot to add modules into DATA_FILES, and since there's actually no
    reason to force users to list them manually when we can just scan
    gluetool_modules and just use everything we find.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # import collections
-# import os
 # import sys
 
+import os
 from setuptools import setup
 
 
@@ -61,179 +61,37 @@ DEPENDENCY_LINKS = [
     #    "http://cdn.mysql.com/Downloads/Connector-Python/mysql-connector-python-2.0.4.zip#md5=3df394d89300db95163f17c843ef49df&egg=mysql-connector-python-2.0.4"  # Ignore PEP8Bear
 ]
 
-DATA_FILES = [
-    [
-        "gluetool_modules/static_analysis/rpmdiff",
-        [
-            "gluetool_modules/static_analysis/rpmdiff/rpmdiff_job.py",
-            "gluetool_modules/static_analysis/rpmdiff/rpmdiff.py",
-            "gluetool_modules/static_analysis/rpmdiff/rpmdiff_job.moduleinfo",
-            "gluetool_modules/static_analysis/rpmdiff/rpmdiff.moduleinfo"
-        ]
-    ],
-    [
-        "gluetool_modules/static_analysis/covscan",
-        [
-            "gluetool_modules/static_analysis/covscan/covscan_job.py",
-            "gluetool_modules/static_analysis/covscan/covscan.py",
-            "gluetool_modules/static_analysis/covscan/covscan_job.moduleinfo",
-            "gluetool_modules/static_analysis/covscan/covscan.moduleinfo"
-        ]
-    ],
-    [
-        "gluetool_modules/database",
-        [
-            "gluetool_modules/database/postgresql.py",
-            #           "gluetool_modules/database/_mysql.py",
-            "gluetool_modules/database/postgresql.moduleinfo",
-            #            "gluetool_modules/database/_mysql.moduleinfo"
-        ]
-    ],
-    [
-        "gluetool_modules/helpers",
-        [
-            "gluetool_modules/helpers/rpmdiff_waiver.py",
-            "gluetool_modules/helpers/publisher_umb_bus.py",
-            "gluetool_modules/helpers/pipeline_state_reporter.py",
-            "gluetool_modules/helpers/exporter_resultsdb.py",
-            "gluetool_modules/helpers/envinject.py",
-            "gluetool_modules/helpers/notify_recipients.py",
-            "gluetool_modules/helpers/guess_openstack_image.py",
-            "gluetool_modules/helpers/guest_setup.py",
-            "gluetool_modules/helpers/publisher_ci_bus.py",
-            "gluetool_modules/helpers/brew_tag_build.py",
-            "gluetool_modules/helpers/guess_beaker_distro.py",
-            "gluetool_modules/helpers/testing_thread.py",
-            "gluetool_modules/helpers/simple_wow_export.py",
-            "gluetool_modules/helpers/build_dependencies.py",
-            "gluetool_modules/helpers/ansible.py",
-            "gluetool_modules/helpers/guess_product.py",
-            "gluetool_modules/helpers/execute_command.py",
-            "gluetool_modules/helpers/trigger_message.py",
-            "gluetool_modules/helpers/rules_engine.py",
-            "gluetool_modules/helpers/rpmdiff_waiver.moduleinfo",
-            "gluetool_modules/helpers/publisher_umb_bus.moduleinfo",
-            "gluetool_modules/helpers/pipeline_state_reporter.moduleinfo",
-            "gluetool_modules/helpers/exporter_resultsdb.moduleinfo",
-            "gluetool_modules/helpers/envinject.moduleinfo",
-            "gluetool_modules/helpers/notify_recipients.moduleinfo",
-            "gluetool_modules/helpers/guess_openstack_image.moduleinfo",
-            "gluetool_modules/helpers/guest_setup.moduleinfo",
-            "gluetool_modules/helpers/publisher_ci_bus.moduleinfo",
-            "gluetool_modules/helpers/brew_tag_build.moduleinfo",
-            "gluetool_modules/helpers/guess_beaker_distro.moduleinfo",
-            "gluetool_modules/helpers/testing_thread.moduleinfo",
-            "gluetool_modules/helpers/simple_wow_export.moduleinfo",
-            "gluetool_modules/helpers/build_dependencies.moduleinfo",
-            "gluetool_modules/helpers/ansible.moduleinfo",
-            "gluetool_modules/helpers/guess_product.moduleinfo",
-            "gluetool_modules/helpers/execute_command.moduleinfo",
-            "gluetool_modules/helpers/trigger_message.moduleinfo",
-            "gluetool_modules/helpers/rules_engine.moduleinfo"
-        ]
-    ],
-    [
-        "gluetool_modules/helpers/notify_email",
-        [
-            "gluetool_modules/helpers/notify_email/notify_email.py",
-            "gluetool_modules/helpers/notify_email/notify_email.moduleinfo",
-            "gluetool_modules/helpers/notify_email/notify_email_beah_formatter.py",
-            "gluetool_modules/helpers/notify_email/notify_email_beah_formatter.moduleinfo",
-            "gluetool_modules/helpers/notify_email/notify_email_covscan_formatter.py",
-            "gluetool_modules/helpers/notify_email/notify_email_covscan_formatter.moduleinfo",
-            "gluetool_modules/helpers/notify_email/notify_email_rpmdiff_formatter.py",
-            "gluetool_modules/helpers/notify_email/notify_email_rpmdiff_formatter.moduleinfo"
-        ]
-    ],
-    [
-        "gluetool_modules/infrastructure",
-        [
-            "gluetool_modules/infrastructure/bus_listener.py",
-            "gluetool_modules/infrastructure/openstack.py",
-            "gluetool_modules/infrastructure/koji_fedora.py",
-            "gluetool_modules/infrastructure/jenkins.py",
-            "gluetool_modules/infrastructure/_docker.py",
-            "gluetool_modules/infrastructure/bus_listener.moduleinfo",
-            "gluetool_modules/infrastructure/openstack.moduleinfo",
-            "gluetool_modules/infrastructure/koji_fedora.moduleinfo",
-            "gluetool_modules/infrastructure/jenkins.moduleinfo",
-            "gluetool_modules/infrastructure/_docker.moduleinfo"
-        ]
-    ],
-    [
-        "gluetool_modules/dispatchers",
-        [
-            "gluetool_modules/dispatchers/task_dispatcher.py",
-            "gluetool_modules/dispatchers/test_batch_planner.py",
-            "gluetool_modules/dispatchers/task_dispatcher.moduleinfo",
-            "gluetool_modules/dispatchers/test_batch_planner.moduleinfo"
-        ]
-    ],
-    [
-        "gluetool_modules/build_on_commit",
-        [
-            "gluetool_modules/build_on_commit/build_on_commit.py",
-            "gluetool_modules/build_on_commit/build_on_commit.moduleinfo"
-        ]
-    ],
-    [
-        "gluetool_modules/testing/openstack",
-        [
-            "gluetool_modules/testing/openstack/openstack_job.py",
-            "gluetool_modules/testing/openstack/openstack_job.moduleinfo"
-        ]
-    ],
-    [
-        "gluetool_modules/helpers/jenkins",
-        [
-            "gluetool_modules/helpers/jenkins/jenkins_build_name.py",
-            "gluetool_modules/helpers/jenkins/jenkins_build_name.moduleinfo"
-        ]
-    ],
-    [
-        "gluetool_modules/testing",
-        [
-            "gluetool_modules/testing/testing_results.py",
-            "gluetool_modules/testing/mtf.py",
-            "gluetool_modules/testing/beah_result_parser.py",
-            "gluetool_modules/testing/wow.py",
-            "gluetool_modules/testing/beah_xunit.py",
-            "gluetool_modules/testing/testing_results.moduleinfo",
-            "gluetool_modules/testing/mtf.moduleinfo",
-            "gluetool_modules/testing/beah_result_parser.moduleinfo",
-            "gluetool_modules/testing/wow.moduleinfo",
-            "gluetool_modules/testing/beah_xunit.moduleinfo"
-        ]
-    ],
-    [
-        "gluetool_modules/testing/beaker",
-        [
-            "gluetool_modules/testing/beaker/beaker_job.py",
-            "gluetool_modules/testing/beaker/beaker.py",
-            "gluetool_modules/testing/beaker/beaker_job.moduleinfo",
-            "gluetool_modules/testing/beaker/beaker.moduleinfo"
-        ]
-    ],
-    [
-        "gluetool_modules/testing/restraint",
-        [
-            "gluetool_modules/testing/restraint/restraint.py",
-            "gluetool_modules/testing/restraint/scheduler.py",
-            "gluetool_modules/testing/restraint/runner.py",
-            "gluetool_modules/testing/restraint/restraint.moduleinfo",
-            "gluetool_modules/testing/restraint/scheduler.moduleinfo",
-            "gluetool_modules/testing/restraint/runner.moduleinfo"
-        ]
-    ],
-    [
-        "gluetool_modules/provision",
-        [
-            "gluetool_modules/provision/docker.py",
-            "gluetool_modules/provision/docker.moduleinfo"
-        ]
+
+# Generate list of data files - modules and their moduleinfo files - for a category.
+# That is basically a list of all files in a directory (which *is* the category name)
+# under gluetool_modules. Ignore other extensions, __init__.py - these are not modules
+# - and also skip _mysql module which has dependency issues :/ And sort those data files.
+#
+# Output is what setup expects to get when dealing with data files:
+# [data file directory, [data file #1, data file #2, ...]]
+def _data_files(category_slug):
+    dirpath = os.path.join('gluetool_modules', category_slug)
+
+    return [
+        dirpath,
+        sorted([
+            os.path.join(dirpath, filepath) for filepath in os.listdir(dirpath)
+            # pylint: disable=line-too-long
+            if (filepath.endswith('.py') or filepath.endswith('.moduleinfo')) and filepath != '__init__.py' and not filepath.startswith('_mysql.')
+        ])
     ]
+
+
+# List of module categories, aka. subdirectories of gluetool_modules.
+CATEGORIES = [
+    'build_on_commit', 'database', 'dispatchers', 'helpers', 'helpers/jenkins', 'helpers/notify_email',
+    'infrastructure', 'provision', 'static_analysis', 'static_analysis/covscan', 'static_analysis/rpmdiff',
+    'testing', 'testing/beaker', 'testing/openstack', 'testing/restraint'
 ]
 
+DATA_FILES = [
+    _data_files(category) for category in CATEGORIES
+]
 
 # def get_install_requires():
 #    requirements = []
