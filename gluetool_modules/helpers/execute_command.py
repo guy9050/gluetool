98f60f4e2294d5e27657c8c8d5e85811fcec2875
Milos Prchlik <mprchlik@redhat.com>
2018-06-08 09:42:21 +0200

    execute-command: added support eval_context and running whole scripts
    E.g. following command should be now easily in module's reach:
    
    scp -r foo bar@baz:{{ PRIMARY_TASK.component_id }}/{{ PRIMARY_TASK.version }}-{{ PRIMARY_TASK.release }}/{{ JENKINS_BUILD_ID }}/
    
    There are some options for moving artifacts from workspace to other storages,
    but they usualy appen when citool-powered pipeline has finished its job, and
    have no access to variables like PRIMARY_TASK.version. Constructing more
    complicated commands based on artifact knowledge is not really possible with
    these external methods.

--- gluetool_modules/helpers/execute_command.py
+++ gluetool_modules/helpers/execute_command.py
@@ -17,14 +17,45 @@ class ExecuteCommand(gluetool.Module):
             'type': str,
             'action': 'append',
             'default': []
+        },
+        'script': {
+            'help': 'Script - YAML file with a list of commands - to execute.',
+            'type': str,
+            'action': 'append',
+            'default': []
         }
     }
 
-    required_options = ('command',)
+    def sanity(self):
+        if not self.option('command') and not self.option('script'):
+            raise gluetool.GlueError("You have to use either `--command` or `--script`")
+
+        if self.option('command') and self.option('script'):
+            raise gluetool.GlueError("You have to use just one of `--command` or `--script`")
 
     def execute(self):
-        for command in self.option('command'):
-            self.info('Running: {}'.format(command))
+        commands = []
+
+        if self.option('command'):
+            commands = self.option('command')
+
+        else:
+            for script in gluetool.utils.normalize_path_option(self.option('script')):
+                script_commands = gluetool.utils.load_yaml(script, logger=self.logger)
+
+                if not isinstance(script_commands, list):
+                    raise gluetool.GlueError("Script '{}' does not contain a list of commands".format(script))
+
+                commands += script_commands
+
+        context = self.shared('eval_context')
+
+        for command in commands:
+            original_command = command
+
+            self.info('Running command: {}'.format(command))
+
+            command = gluetool.utils.render_template(command, logger=self.logger, **context)
 
             split_command = shlex.split(command)
 
@@ -39,4 +70,4 @@ class ExecuteCommand(gluetool.Module):
             gluetool.log.log_blob(self.error, 'stderr', output.stderr)
 
             if output.exit_code != 0:
-                raise gluetool.GlueError("Command '{}' exited with non-zero exit code".format(command))
+                raise gluetool.GlueError("Command '{}' exited with non-zero exit code".format(original_command))
