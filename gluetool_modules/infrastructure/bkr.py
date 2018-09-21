3a097daf8a565099a3a05433f27f9df3cad75cfb
Milos Prchlik <mprchlik@redhat.com>
2018-09-21 12:35:48 +0200

    bkr: provide job results and matrix URL parser

--- gluetool_modules/infrastructure/bkr.py
+++ gluetool_modules/infrastructure/bkr.py
@@ -1,9 +1,14 @@
+import collections
 import json
 import os
 import tempfile
+import urlparse
+
+import bs4
 
 import gluetool
 from gluetool import GlueError, GlueCommandError
+from gluetool.utils import Command
 from libci.sentry import PrimaryTaskFingerprintsMixin
 
 
@@ -21,6 +26,13 @@ class InvalidTasksError(PrimaryTaskFingerprintsMixin, GlueError):
         self.tasks = test_tasks
 
 
+#: Represents (interesting) building pieces of Beaker matrix URL
+#:
+#: :ivar bool toggle_nacks_on: whether display of NACKed jobs is enabled.
+#: :ivar list(int) job_ids: list of Beaker job IDs to display.
+MatrixURL = collections.namedtuple('MatrixURL', ('toggle_nacks_on', 'job_ids'))
+
+
 class Beaker(gluetool.Module):
     """
     Provides a wrapper of `bkr` command, allowing for easier integration with other modules.
@@ -29,7 +41,7 @@ class Beaker(gluetool.Module):
     name = 'bkr'
     description = 'Wrapper of (low-level) Beaker API and commands.'
 
-    shared_functions = ('submit_beaker_jobs',)
+    shared_functions = ('submit_beaker_jobs', 'beaker_jobs_results', 'parse_beaker_matrix_url')
 
     def _submit_job(self, job):
         """
@@ -51,7 +63,7 @@ class Beaker(gluetool.Module):
 
         # submit the job to beaker
         try:
-            output = gluetool.utils.Command(['bkr'], options=['job-submit', job_file.name], logger=self.logger).run()
+            output = Command(['bkr'], options=['job-submit', job_file.name], logger=self.logger).run()
 
         except GlueCommandError as exc:
             if 'Invalid task(s):' in exc.output.stderr:
@@ -87,7 +99,6 @@ class Beaker(gluetool.Module):
         return ids
 
     def submit_beaker_jobs(self, jobs):
-        # pylint: disable=no-self-use
         """
         Submit jobs to Beaker.
 
@@ -103,3 +114,61 @@ class Beaker(gluetool.Module):
         gluetool.log.log_dict(self.debug, 'beaker job IDs', beaker_ids)
 
         return beaker_ids
+
+    def beaker_jobs_results(self, jobs):
+        """
+        Find and return results of Beaker jobs. The "result" in question is an XML, returned
+        by ``bkr job-results`` command.
+
+        :param list(int) jobs: List of job IDs to query.
+        :rtype: dict(int, xml)
+        :returns: a mapping between job IDs (those given by in ``jobs`` argument) and their respective
+            results.
+        """
+
+        # `bkr job-results` prints XML on its stdout - for each job, capture that text
+        # and convert it to an XML tree.
+        results = {
+            job: bs4.BeautifulSoup(Command(['bkr'],
+                                           options=['job-results', 'J:{}'.format(job)],
+                                           logger=self.logger).run().stdout, 'xml')
+            for job in jobs
+        }
+
+        for job, result in results.iteritems():
+            gluetool.log.log_xml(self.verbose, 'job {} result'.format(job), result)
+
+        return results
+
+    def parse_beaker_matrix_url(self, url):
+        """
+        Split and normalize parts of Beaker matrix URL.
+
+        :param str url: URL to process.
+        :returns: An object with following properties:
+            * ``toggle_nacks_on`` (``bool``) - whether display of NACKs was enabled
+            * ``job_ids`` (``list(int)``) - list of job IDs to display
+        """
+
+        self.debug("parsing Beaker matrix URL '{}'".format(url))
+
+        # parse matrix URL to its components (schema, hostname, port, query, ...)
+        parsed = urlparse.urlparse(url)
+        self.debug('  parsed: {}'.format(parsed))
+
+        # extract "query" part of URL in a form of a mapping
+        query = urlparse.parse_qs(parsed.query)
+        gluetool.log.log_dict(self.debug, '  query', query)
+
+        # `togle_nacks_on` value is a list with a single string item, 'on' or 'off'
+        toggle_nacks_on = query.get('toggle_nacks_on', ['off'])[0].lower().strip() == 'on'
+        self.debug('  toggle_nacks_on={}'.format(toggle_nacks_on))
+
+        # job_ids is a list of all IDs packed into a single string, separated by a space
+        job_ids = [
+            int(job_id)
+            for job_id in gluetool.utils.normalize_multistring_option(query.get('job_ids', []), separator=' ')
+        ]
+        gluetool.log.log_dict(self.debug, '  job_ids', job_ids)
+
+        return MatrixURL(toggle_nacks_on=toggle_nacks_on, job_ids=job_ids)
