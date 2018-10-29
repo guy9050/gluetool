4ec843c7c7a5c431dd303f732e7506e43f5797b8
Martin Kluson <mkluson@redhat.com>
2018-10-29 13:34:06 +0100

    copr: NVR check
    Raise `SUTInstallationFailedError` when intallation of any of packages
    fails.
    
    Requres: https://gitlab.cee.redhat.com/baseos-qe/citool-config/merge_requests/382
    and patch on ansible, which will come soon.

--- gluetool_modules/helpers/install_copr_build.py
+++ gluetool_modules/helpers/install_copr_build.py
@@ -1,5 +1,17 @@
 import gluetool
 from gluetool.log import log_dict
+from gluetool import SoftGlueError
+from libci.sentry import PrimaryTaskFingerprintsMixin
+# pylint: disable=no-name-in-module
+from jq import jq
+
+
+class SUTInstallationFailedError(PrimaryTaskFingerprintsMixin, SoftGlueError):
+    def __init__(self, task, guest, packages):
+        super(SUTInstallationFailedError, self).__init__(task, 'SUT installation failed')
+
+        self.guest = guest
+        self.packages = packages
 
 
 class InstallCoprBuild(gluetool.Module):
@@ -34,7 +46,41 @@ class InstallCoprBuild(gluetool.Module):
         log_dict(self.debug, 'RPMs to install', rpm_names)
         log_dict(self.debug, 'RPMs install from', rpm_urls)
 
-        self.shared('run_playbook', gluetool.utils.normalize_path(self.option('playbook')), guests, variables={
-            'PACKAGE_URLS': rpm_urls,
-            'PACKAGE_NAMES': rpm_names
-        })
+        ansible_output = self.shared(
+            'run_playbook',
+            gluetool.utils.normalize_path(self.option('playbook')),
+            guests,
+            variables={
+                'PACKAGE_URLS': rpm_urls,
+                'PACKAGE_NAMES': rpm_names
+            },
+            json_output=True
+        )
+
+        query = """
+              .plays[].tasks[]
+            | select(.task.name == "NVR check")
+            | .hosts | to_entries[]
+            | {
+                host: .key,
+                packages: [
+                    .value.results[]
+                  | select(.failed == true)
+                  | .item
+                ]
+              }
+            | select(.packages != [])""".replace('\n', '')
+
+        failed_tasks = jq(query).transform(ansible_output, multiple_output=True)
+
+        log_dict(self.debug, 'ansible output after jq processing', failed_tasks)
+
+        if failed_tasks:
+            first_fail = failed_tasks[0]
+            guest = [guest for guest in guests if guest.hostname == first_fail['host']][0]
+            packages = first_fail['packages']
+
+            guest.warn('Packages {} have not been installed.'.format(','.join(packages)))
+            raise SUTInstallationFailedError(self.shared('primary_task'), guest, packages)
+
+        self.info('All packages have been successfully installed')
