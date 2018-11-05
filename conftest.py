5bae3a443939d5dde03a896cf789472f20affd35
Milos Prchlik <mprchlik@redhat.com>
2018-11-05 11:34:00 +0100

    execute-command: unit tests

--- conftest.py
+++ conftest.py
@@ -1,4 +1,5 @@
 # pylint: disable=unused-import
+import mock
 import pytest
 
 from gluetool.tests.conftest import fixture_enable_logger, fixture_enable_logger_propagate, fixture_log  # noqa
@@ -13,3 +14,33 @@ def pytest_addoption(parser):
 @pytest.fixture(name='integration_config')
 def fixture_integration_config(request):
     return request.config.getoption('--integration-config')
+
+
+@pytest.fixture(name='mock_command')
+def fixture_mock_command(monkeypatch):
+    """
+    Mock :py:ref:`gluetool.utils.Command` and its ``run`` method.
+
+    :returns: tuple of 4 items:
+        * a mock representing ``Command()`` call,
+        * a mock representing new ``Command`` instance, returned by this ``Command()`` call,
+        * a mock representing ``run`` method of this instance, and
+        * a mock output returned when this ``run()`` is called.
+    """
+
+    # `Command.run` return value
+    mock_output = mock.MagicMock(exit_code=0, stdout='dummy stdout', stderr='dummy stderr')
+
+    # `Command.run` method
+    mock_run = mock.MagicMock(return_value=mock_output)
+
+    # `Command` instance
+    mock_command = mock.MagicMock(run=mock_run)
+
+    # `Command()` mock
+    mock_command_init = mock.MagicMock(return_value=mock_command)
+
+    import gluetool
+    monkeypatch.setattr(gluetool.utils, 'Command', mock_command_init)
+
+    return mock_command_init, mock_command, mock_run, mock_output
