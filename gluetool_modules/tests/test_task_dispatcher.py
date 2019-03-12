e882ef30a30c1ea38885b833f89766fd6b655454
Martin Kluson <mkluson@redhat.com>
2019-03-12 09:46:26 +0100

    add tests dor test-task-dispatcher

---
+++ gluetool_modules/tests/test_task_dispatcher.py
@@ -0,0 +1,208 @@
+import pytest
+import gluetool
+from mock import MagicMock
+import gluetool_modules.dispatchers.task_dispatcher
+from gluetool_modules.dispatchers.task_dispatcher import TaskDispatcher
+from . import create_module, patch_shared
+
+
+@pytest.fixture(name='module')
+def fixture_module():
+    module = create_module(TaskDispatcher)[1]
+    return module
+
+
+@pytest.fixture(name='module_run_module')
+def fixture_module_run_module(module, monkeypatch):
+    run_module_mock = MagicMock()
+
+    monkeypatch.setattr(
+        'gluetool.Module.run_module',
+        run_module_mock
+    )
+
+    return module, run_module_mock
+
+
+def test_loadable(module):
+    ci = module.glue
+    python_mod = ci._load_python_module('dispatchers/task_dispatcher.py', 'pytest_task_dispatcher',
+                                        'gluetool_modules/dispatchers/task_dispatcher.py')
+
+    assert hasattr(python_mod, 'TaskDispatcher')
+
+
+def test_empty_test_batch(module_run_module, monkeypatch):
+    module, run_module_mock = module_run_module
+
+    patch_shared(monkeypatch, module, {
+        'plan_test_batch': []
+    })
+
+    module.execute()
+
+    assert not run_module_mock.called
+
+
+def test_execute(module_run_module, monkeypatch):
+    module, run_module_mock = module_run_module
+
+    module_name = 'module1'
+    options = ['--option1', '--option2']
+    test_batch = [(module_name, options)]
+
+    patch_shared(monkeypatch, module, {
+        'plan_test_batch': test_batch
+    })
+
+    module.execute()
+
+    run_module_mock.assert_called_with(module_name, options)
+
+
+def test_task_id(module_run_module, monkeypatch):
+    module, run_module_mock = module_run_module
+
+    module_name = 'module1'
+    options = ['--option1', '--option2']
+    test_batch = [(module_name, options)]
+
+    patch_shared(monkeypatch, module, {
+        'plan_test_batch': test_batch,
+        'thread_id': 1234
+    })
+
+    module.execute()
+
+    run_module_mock.assert_called_with(module_name, ['--testing-thread-id', '1234-1'] + options)
+
+
+def test_state_reporter_unknown_options(module_run_module, monkeypatch):
+    module, run_module_mock = module_run_module
+
+    module_name = 'module1'
+    options = ['--option1', '--option2']
+    test_batch = [(module_name, options)]
+
+    topic = 'dummy_topic'
+    module._config['pipeline-test-bus-topic'] = topic
+
+    report_pipeline_state_mock = MagicMock()
+    monkeypatch.setattr(module.glue, 'shared_functions', {
+        'report_pipeline_state': (None, report_pipeline_state_mock),
+        'plan_test_batch': (None, MagicMock(return_value=test_batch))
+    })
+
+    module.execute()
+
+    report_pipeline_state_mock.assert_called_with(
+        'queued',
+        thread_id=None,
+        topic=topic,
+        test_category='unknown',
+        test_type='unknown'
+    )
+    run_module_mock.assert_called_with(module_name, options)
+
+
+def test_state_reporter_options_from_args(module_run_module, monkeypatch):
+    module, run_module_mock = module_run_module
+
+    module_name = 'module1'
+    options = ['--pipeline-state-reporter-options="--test-type dummy_test_type --test-category dummy_test_category"']
+    test_batch = [(module_name, options)]
+
+    topic = 'dummy_topic'
+    module._config['pipeline-test-bus-topic'] = topic
+
+    report_pipeline_state_mock = MagicMock()
+    monkeypatch.setattr(module.glue, 'shared_functions', {
+        'report_pipeline_state': (None, report_pipeline_state_mock),
+        'plan_test_batch': (None, MagicMock(return_value=test_batch))
+    })
+
+    module.execute()
+
+    report_pipeline_state_mock.assert_called_with(
+        'queued',
+        thread_id=None,
+        topic=topic,
+        test_category='dummy_test_category',
+        test_type='dummy_test_type'
+    )
+    run_module_mock.assert_called_with(module_name, options)
+
+
+def test_state_reporter_options_from_mapping(module_run_module, monkeypatch):
+    module, run_module_mock = module_run_module
+
+    module_name = 'module1'
+    options = ['--option1', '--option2']
+    test_batch = [(module_name, options)]
+
+    topic = 'dummy_topic'
+    module._config['pipeline-test-bus-topic'] = topic
+    module._config['pipeline-test-categories'] = 'dummy_topic_categories'
+    module._config['pipeline-test-types'] = 'dummy_topic_types'
+
+    report_pipeline_state_mock = MagicMock()
+    monkeypatch.setattr(module.glue, 'shared_functions', {
+        'report_pipeline_state': (None, report_pipeline_state_mock),
+        'plan_test_batch': (None, MagicMock(return_value=test_batch))
+    })
+
+    mapping_mock = MagicMock()
+    mapping_mock.match = MagicMock(return_value='dummy_value')
+    monkeypatch.setattr(
+        'gluetool.utils.SimplePatternMap',
+        MagicMock(return_value=mapping_mock)
+    )
+
+    module.execute()
+
+    report_pipeline_state_mock.assert_called_with(
+        'queued',
+        thread_id=None,
+        topic=topic,
+        test_category='dummy_value',
+        test_type='dummy_value'
+    )
+    run_module_mock.assert_called_with(module_name, options)
+
+
+def test_state_reporter_options_mapping_error(module_run_module, monkeypatch):
+    module, run_module_mock = module_run_module
+
+    module_name = 'module1'
+    options = ['--option1', '--option2']
+    test_batch = [(module_name, options)]
+
+    topic = 'dummy_topic'
+    module._config['pipeline-test-bus-topic'] = topic
+    module._config['pipeline-test-categories'] = 'dummy_topic_categories'
+    module._config['pipeline-test-types'] = 'dummy_topic_types'
+
+    report_pipeline_state_mock = MagicMock()
+    monkeypatch.setattr(module.glue, 'shared_functions', {
+        'report_pipeline_state': (None, report_pipeline_state_mock),
+        'plan_test_batch': (None, MagicMock(return_value=test_batch))
+    })
+
+    mapping_mock = MagicMock()
+    mapping_mock.match = MagicMock()
+    mapping_mock.match.side_effect = gluetool.GlueError('dummy_error')
+    monkeypatch.setattr(
+        'gluetool.utils.SimplePatternMap',
+        MagicMock(return_value=mapping_mock)
+    )
+
+    module.execute()
+
+    report_pipeline_state_mock.assert_called_with(
+        'queued',
+        thread_id=None,
+        topic=topic,
+        test_category='unknown',
+        test_type='unknown'
+    )
+    run_module_mock.assert_called_with(module_name, options)
