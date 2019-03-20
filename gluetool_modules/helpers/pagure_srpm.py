6c914cd9d1aa00c61cffcdeeab5cf621a5612dd2
Martin Kluson <mkluson@redhat.com>
2019-03-20 11:01:17 +0100

    pagure-srpm: allow unrelated histories while merging

--- gluetool_modules/helpers/pagure_srpm.py
+++ gluetool_modules/helpers/pagure_srpm.py
@@ -1,4 +1,5 @@
 import os
+import shlex
 import gluetool
 from gluetool.utils import Command
 
@@ -7,6 +8,21 @@ class PagureSRPM(gluetool.Module):
 
     name = 'pagure-srpm'
     description = 'Makes source rpm from pagure pull request'
+
+    options = {
+        'git-clone-options': {
+            'help': 'Additional options for `git clone` command (default: %(default)s).',
+            'default': ''
+        },
+        'git-fetch-options': {
+            'help': 'Additional options for `git fetch` command (default: %(default)s).',
+            'default': ''
+        },
+        'git-merge-options': {
+            'help': 'Additional options for `git merge` command (default: %(default)s).',
+            'default': ''
+        },
+    }
 
     shared_functions = ['src_rpm']
 
@@ -18,22 +34,25 @@ class PagureSRPM(gluetool.Module):
         if pull_request.ARTIFACT_NAMESPACE not in ['dist-git-pr']:
             raise gluetool.GlueError('Incompatible artifact namespace: {}'.format(pull_request.ARTIFACT_NAMESPACE))
 
-        git_clone_cmd = [
+        clone_cmd = [
             'git', 'clone',
             '-b', pull_request.destination_branch,
             pull_request.project.clone_url
         ]
-        Command(git_clone_cmd, logger=self.logger).run()
+        clone_cmd.extend(shlex.split(self.option('git-clone-options')))
+        Command(clone_cmd, logger=self.logger).run()
 
         os.chdir(pull_request.project.name)
 
         pr_id = pull_request.pull_request_id.repository_pr_id
 
         fetch_cmd = ['git', 'fetch', 'origin', 'refs/pull/{}/head'.format(pr_id)]
+        fetch_cmd.extend(shlex.split(self.option('git-fetch-options')))
         Command(fetch_cmd, logger=self.logger).run()
 
         merge_cmd = ['git', 'merge', 'FETCH_HEAD', '-m', 'ci pr merge']
-        Command(merge_cmd, logger=self.logger).run()
+        merge_cmd.extend(shlex.split(self.option('git-merge-options')))
+        Command(merge_cmd, logger=self.logger).run(inspect=True)
 
         last_comment_id = pull_request.comments[-1]['id'] if pull_request.comments else 0
 
