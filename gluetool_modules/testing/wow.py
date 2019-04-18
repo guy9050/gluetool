84c99ac0fcda9b7d0607551ac189b8d737a17ae6
Milos Prchlik <mprchlik@redhat.com>
2019-04-18 13:53:44 +0200

    wow: moving --decision option to configuration
    Some XML generators may lack the support, and there's no workaround.

--- gluetool_modules/testing/wow.py
+++ gluetool_modules/testing/wow.py
@@ -326,8 +326,7 @@ class WorkflowTomorrow(gluetool.Module):
             )
 
             command = WowCommand(['bkr', 'workflow-tomorrow'], [
-                '--dry-run',  # this will make wow to print job description in XML
-                '--decision'  # show desicions about including/not including task in the job
+                '--dry-run'  # this will make wow to print job description in XML
             ], logger=self.logger)
 
             def _add_note(instruction, command, argument, context):
