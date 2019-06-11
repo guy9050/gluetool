260b7fc5cdaabdf971de9ad13583f61559a67377
mprchlik <mprchlik@redhat.com>
2019-06-11 17:32:10 +0530

    wow-artifact-installation-options-koji-build: order installation tasks
    wow sorts them for us, but there are tools that don't understand
    --init-task and --first-testing-task and may change them to pure --task
    options. In that case, the order we used would lead to wrong results
    (checking before installation).

--- gluetool_modules/helpers/beaker/wow_artifact_installation_options_koji_build.py
+++ gluetool_modules/helpers/beaker/wow_artifact_installation_options_koji_build.py
@@ -22,10 +22,6 @@ class WowArtifactInstallationOptionsKojiBuild(gluetool.Module):
 
         self.require_shared('brew_build_task_params')
 
-        options = [
-            '--first-testing-task=/distribution/runtime_tests/verify-nvr-installed'
-        ]
-
         # create options for brew-build task
         brew_build_task_params = self.shared('brew_build_task_params')
 
@@ -35,10 +31,17 @@ class WowArtifactInstallationOptionsKojiBuild(gluetool.Module):
             '{}="{}"'.format(param, value) for param, value in brew_build_task_params.iteritems()
         ])
 
-        # this should be '--init-task="{} /....', but it's not a problem for python-based programs (bkr)
-        # and sclrun can handle it too
-        options += [
-            '--init-task={} /distribution/install/brew-build'.format(brew_build_task_params)
+        options = [
+            # This should be '--init-task="{} /....', but it's not a problem for python-based programs (bkr)
+            # and sclrun can handle it too
+            '--init-task={} /distribution/install/brew-build'.format(brew_build_task_params),
+
+            # We should keep the order of "install" - "verify" steps - wow may rewrite these options
+            # to use something better supported by the actual tool used (e.g. wow-tomorrow vs. wow-autofs),
+            # e.g. by converting these `-foo-task` options into pure `--task` options, we should help
+            # by using them in a correct order since we *do* know their order is important (even when
+            # we don't have to do it for wow).
+            '--first-testing-task=/distribution/runtime_tests/verify-nvr-installed'
         ]
 
         log_dict(self.debug, 'wow options for Brew/Koji artifact installation', options)
