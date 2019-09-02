39bd0e30b98de7911bb971b2d7ee262c4181e7fc
Milos Prchlik <mprchlik@redhat.com>
2019-09-02 21:08:56 +0200

    Install citool directly from its repository
    Not using pypi index anymore, to avoid need for persistent storage. We were using it just for this single package anyway, waste of time.

--- setup.py
+++ setup.py
@@ -140,7 +140,7 @@ if __name__ == '__main__':
           data_files=DATA_FILES,
           install_requires=[
               'gluetool',
-              'citool',
+              'citool @ git+https://gitlab.cee.redhat.com/baseos-qe/citool.git#egg=citool',
               'sphinx-argparse==0.2.0',
               'sphinxcontrib-programoutput==0.11'
           ] + INSTALL_REQUIRES,
