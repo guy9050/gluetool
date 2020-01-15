75baeab10e624f2ce572dbb1b5109377549b63d6
Martin Kluson <mkluson@redhat.com>
2020-01-15 19:03:21 +0100

    osci-compose-url: new module for obtaining OSCI compose url.

--- gluetool_modules/testing/test_scheduler_upgrades.py
+++ gluetool_modules/testing/test_scheduler_upgrades.py
@@ -1,9 +1,7 @@
 import gluetool
 import re
-import ftplib
 
 from gluetool.log import log_dict
-from six.moves.urllib.parse import urlsplit
 from gluetool_modules.libs.artifacts import splitFilename
 
 
@@ -12,13 +10,6 @@ class TestSchedulerUpgrades(gluetool.Module):
     name = 'test-scheduler-upgrades'
     description = 'Prepare schedule for upgrade testing. Modify schedule entries provided by previous (STI) provider.'
 
-    options = {
-        'compose-server-address': {
-            'help': 'address of server, where OSCI composes are stored'
-        },
-    }
-
-    required_options = ('compose-server-address',)
     shared_functions = ['create_test_schedule']
 
     def nvr_to_nevr_pattern(self, nvr, component):
@@ -37,57 +28,7 @@ class TestSchedulerUpgrades(gluetool.Module):
         self.require_shared('primary_task')
         primary_task = self.shared('primary_task')
 
-        matched_target = re.match(r'(rhel-\d\.\d\.\d(?:-z)?)-.*', primary_task.destination_tag)
-
-        if matched_target is None:
-            raise gluetool.GlueError('Unsupported primary task target: {}'.format(primary_task.target))
-
-        composes_subdir_path = 'comp/{}'.format(matched_target.group(1))
-
-        # Url of compose is found by name.
-        # Name has following structure: {task_id}-{compose_id}-{component_name}
-        # Based on known `task_id` and `component_name` most recent compose (with highest `compose_id`) is found.
-
-        compose_name_regex = re.compile(r'{}/{}-(\d+)-{}'.format(
-            re.escape(composes_subdir_path),
-            re.escape(str(primary_task.id)),
-            re.escape(primary_task.component)
-        ))
-
-        compose_server_url = self.option('compose-server-address')
-
-        ftp = ftplib.FTP(urlsplit(compose_server_url).netloc)
-        ftp.login()
-        composes = ftp.nlst(composes_subdir_path)
-
-        # Apply pattern to all composes. This will yield some "no match" values (match returns None in such case), but
-        # we'll get rid of them later.
-        matches = [
-            (compose, compose_name_regex.match(compose))
-            for compose in composes
-        ]
-
-        # Find the biggest match group, but consider only actual matches, skipping those "no match" values mentioned
-        # above.
-        try:
-            winning_pair = max(
-                [(compose, match) for compose, match in matches if match],
-                key=lambda pair: int(pair[1].group(1))
-            )
-        except ValueError:
-            raise gluetool.SoftGlueError(
-                "Unable to find OSCI compose for {} ({}) in '{}' directory".format(
-                    primary_task.component,
-                    primary_task.id,
-                    composes_subdir_path
-                )
-            )
-
-        compose = winning_pair[0]
-
-        primary_task_compose_url = '{}/{}'.format(compose_server_url, compose)
-
-        self.info('OSCI compose found: {}'.format(primary_task_compose_url))
+        primary_task_compose_url = self.shared('get_compose_url')
 
         # List of binary package names is obtained from componse metadata (metadata/rpms.json).
 
